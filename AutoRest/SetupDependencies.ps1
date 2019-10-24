31eaaaa1651017bff08cfdfe0dea7baa6425e0b0
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2019-10-24 11:01:23 -0600

    initial commit

---
+++ AutoRest/SetupDependencies.ps1
@@ -0,0 +1,15 @@
+$RequiredModules = ('powershell-yaml', 'BuildHelpers')
+ForEach ($RequiredModule In $RequiredModules)
+{
+    # Check to see if the module is installed
+    If (Get-InstalledModule -Name:($RequiredModule) -ErrorAction:('SilentlyContinue'))
+    {
+        Update-Module -Name:($RequiredModule) -Force
+    }
+    # Check to see if the module exists on the PSGallery
+    ElseIf (Find-Module -Name:($RequiredModule))
+    {
+        Install-Module -Name:($RequiredModule) -Force
+    }
+    Import-Module -Name:($RequiredModule) -Force
+}
