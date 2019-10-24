37aa89f3b9137d3be6b18c4a1e291b7554600b1b
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2019-10-24 12:01:49 -0600

    append username

--- AutoRest/ApiTransform.ps1
+++ AutoRest/ApiTransform.ps1
@@ -365,7 +365,7 @@ $ApiHash.GetEnumerator() | ForEach-Object {
                     Try
                     {
                         Invoke-Git -Arguments:('config user.email "' + $env:BUILD_REQUESTEDFOREMAIL + '";')
-                        Invoke-Git -Arguments:('config user.name "' + $env:BUILD_REQUESTEDFOR + '";')
+                        Invoke-Git -Arguments:('config user.name "' + $env:BUILD_REQUESTEDFOR + '-AzPipelines";')
                         Invoke-Git -Arguments:('add -A')
                         Invoke-Git -Arguments:('status')
                         Invoke-Git -Arguments:('commit -m ' + '"Updating OAS spec;[skip ci]";')
