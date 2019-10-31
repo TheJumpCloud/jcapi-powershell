d0ae39c0b7656503c81730bbf1c7e8eb3ae9b889
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2019-10-31 11:12:18 -0600

    debugging

--- AutoRest/BuildAutoRest.ps1
+++ AutoRest/BuildAutoRest.ps1
@@ -135,26 +135,32 @@ ForEach ($API In $APIName)
                 Write-Error ("Path does not exist: $packModulePath")
             }
             $nupkg = Get-ChildItem -Path:($binFolder + $nupkgName)
-            Expand-Archive -Path:($nupkg.FullName) -DestinationPath:($extractedModulePath) -Verbose
-            Remove-Item -Path:($extractedModulePath + '/_rels') -Recurse -Force -Verbose
-            Remove-Item -Path:($extractedModulePath + '/*Content*Types*.xml') -Force -Verbose
-            Remove-Item -Path:($extractedModulePath + '/package') -Force -Recurse -Verbose
-            Remove-Item -Path:($extractedModulePath + '/' + $ModuleName + '.nuspec') -Force -Verbose
+            Expand-Archive -Path:($nupkg.FullName) -DestinationPath:($extractedModulePath)
+            Remove-Item -Path:($extractedModulePath + '/_rels') -Recurse -Force
+            Remove-Item -Path:($extractedModulePath + '/*Content*Types*.xml') -Force
+            Remove-Item -Path:($extractedModulePath + '/package') -Force -Recurse
+            Remove-Item -Path:($extractedModulePath + '/' + $ModuleName + '.nuspec') -Force
         }
         ###########################################################################
         If ($CommitModule)
         {
-            Write-Host('Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
             If ($env:USERNAME -eq 'VssAdministrator')
             {
                 Try
                 {
+                    Write-Host('Here 1')
                     Invoke-Git -Arguments:('config user.email "' + $env:BUILD_REQUESTEDFOREMAIL + '";')
+                    Write-Host('Here 2')
                     Invoke-Git -Arguments:('config user.name "' + $env:BUILD_REQUESTEDFOR + '";')
+                    Write-Host('Here 3')
                     Invoke-Git -Arguments:('add -A')
+                    Write-Host('Here 4')
                     Invoke-Git -Arguments:('status')
+                    Write-Host('Here 5')
                     Invoke-Git -Arguments:('commit -m ' + '"Updating module: ' + $ModuleName + ';[skip ci]";')
+                    Write-Host('Here 6')
                     Invoke-Git -Arguments:('push origin HEAD:refs/heads/' + $env:BUILD_SOURCEBRANCHNAME + ';')
+                    Write-Host('Here 7')
                 }
                 Catch
                 {
