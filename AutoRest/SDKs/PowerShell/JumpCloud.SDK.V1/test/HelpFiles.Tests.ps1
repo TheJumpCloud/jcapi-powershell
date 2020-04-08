bc8bc46ba76ec7a90ed426dd159fbf555127cfce
btstevens <brad.stevens@gmail.com>
2020-04-08 10:59:43 -0600

    missing /

--- AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/test/HelpFiles.Tests.ps1
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/test/HelpFiles.Tests.ps1
@@ -4,7 +4,7 @@ Describe -Tag:('ModuleValidation') 'Help File Tests' {
     $Regex_FillInThe = '(\{\{)(.*?)(\}\})'
     $Regex_FillInThePester = [regex]('{{.*?}}')
     Context ('Validating help file fields have been populated') {
-        Get-ChildItem -Path:($ModuleRootFullName + 'JumpCloud.SDK.V1/Docs/*.md') -Recurse | Select-String -Pattern:($Regex_FillInThe) | ForEach-Object {
+        Get-ChildItem -Path:($ModuleRootFullName + '/JumpCloud.SDK.V1/Docs/*.md') -Recurse | Select-String -Pattern:($Regex_FillInThe) | ForEach-Object {
             It ('The file "' + $_.Path + '" needs to be populated on line number "' + $_.LineNumber + '" where "' + $_.Line + '" exists.') {
                 $_.Path | Should -Not -FileContentMatch ($Regex_FillInThePester)
             }
