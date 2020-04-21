5494a61229001e8a483838e52a83870563305aba
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-04-21 16:53:13 -0600

    testing

--- AutoRest/BuildAutoRest.ps1
+++ AutoRest/BuildAutoRest.ps1
@@ -1,6 +1,6 @@
 #Requires -Modules powershell-yaml, BuildHelpers
 Param(
-    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Name of the API to build an SDK for.')][ValidateSet('V1', 'V2', 'DirectoryInsights')][ValidateNotNullOrEmpty()][System.String[]]$APIName
+    [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Name of the API to build an SDK for.')][ValidateSet('V1', 'V2', 'DirectoryInsights')][ValidateNotNullOrEmpty()][System.String[]]$APIName
     , [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Populate with "beta" to make release a prerelease.')][ValidateSet('beta')][System.String[]]$PrereleaseName
     , [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = 'API key used for pester tests.')][ValidateNotNullOrEmpty()][System.String[]]$JCApiKey
     , [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = 'OrgId used for pester tests.')][ValidateNotNullOrEmpty()][System.String[]]$JCOrgId
@@ -87,7 +87,7 @@ Try
                     $LatestModule = Find-Module -Name:($ModuleName) -Repository:($PSRepoName) -ErrorAction:('SilentlyContinue')
                     If ([System.String]::IsNullOrEmpty($LatestModule))
                     {
-                        $LatestModule = Find-Module -Name:($ModuleName) -Repository:($PSRepoName) -ErrorAction:('SilentlyContinue') -AllowPrerelease
+                        $LatestModule = Find-Module -Name:($ModuleName) -Repository:($PSRepoName) -ErrorAction:('SilentlyContinue')
                     }
                     If (-not [System.String]::IsNullOrEmpty($LatestModule))
                     {
@@ -122,12 +122,12 @@ Try
                 ###########################################################################
                 If ($UpdateModuleManifest)
                 {
-                    # Increment module version number
-                    If (-not [System.String]::IsNullOrEmpty($NextVersion))
-                    {
-                        Write-Host ('[RUN COMMAND] Increment module version number to: ' + $NextVersion) -BackgroundColor:('Black') -ForegroundColor:('Magenta')
-                        Update-ModuleManifest -Path:($moduleManifestPath) -ModuleVersion:($NextVersion)
-                    }
+                    # # Increment module version number
+                    # If (-not [System.String]::IsNullOrEmpty($NextVersion))
+                    # {
+                    #     Write-Host ('[RUN COMMAND] Increment module version number to: ' + $NextVersion) -BackgroundColor:('Black') -ForegroundColor:('Magenta')
+                    #     Update-ModuleManifest -Path:($moduleManifestPath) -ModuleVersion:($NextVersion)
+                    # }
                     # # Get existing GUID
                     # $LatestModule | Install-Module -Force -Scope:('CurrentUser')
                     # Import-Module -Name:($LatestModule.Name) -Force
