18fdf5b36e042656138940b0724a6781ab823778
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-04-23 10:00:30 -0600

    update to work with ProxyCmdletDefinitions.ps1

--- JumpCloud/Build-Module.ps1
+++ JumpCloud/Build-Module.ps1
@@ -1,8 +1,8 @@
-# $UserAgent = Get-JCUserAgent
-# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $OutputPath = $PSScriptRoot + '/JumpCloudV2/'
 $IndentChar = '    '
-$FunctionTemplate = "{0}`nFunction {1}`n{{`n$($IndentChar)#Requires -modules JumpCloud, {2}`n$($IndentChar){3}`n$($IndentChar)Param(`n{4}`n$($IndentChar))`n$($IndentChar)Begin`n$($IndentChar){{`n{5}`n$($IndentChar)}}`n$($IndentChar)Process`n$($IndentChar){{`n{6}`n$($IndentChar)}}`n$($IndentChar)End`n$($IndentChar){{`n{7}`n$($IndentChar)}}`n}}"
+$MSCopyrightHeader = "`n# ----------------------------------------------------------------------------------`n#`n# Copyright Microsoft Corporation`n# Licensed under the Apache License, Version 2.0 (the ""License"");`n# you may not use this file except in compliance with the License.`n# You may obtain a copy of the License at`n# http://www.apache.org/licenses/LICENSE-2.0`n# Unless required by applicable law or agreed to in writing, software`n# distributed under the License is distributed on an ""AS IS"" BASIS,`n# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`n# See the License for the specific language governing permissions and`n# limitations under the License.`n# ----------------------------------------------------------------------------------`n"
+$Divider = '|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|'
+$FunctionTemplate = "{0}`nFunction {1}`n{{`n$($IndentChar)#Requires -modules {2}`n$($IndentChar){3}`n$($IndentChar)Param(`n{4}`n$($IndentChar))`n$($IndentChar)Begin`n$($IndentChar){{`n{5}`n$($IndentChar)}}`n$($IndentChar)Process`n$($IndentChar){{`n{6}`n$($IndentChar)}}`n$($IndentChar)End`n$($IndentChar){{`n{7}`n$($IndentChar)}}`n}}"
 $ScriptAnalyzerResults = @()
 If (Test-Path -Path:($OutputPath))
 {
@@ -10,34 +10,45 @@ If (Test-Path -Path:($OutputPath))
 }
 If (Get-Module -Name($ModuleNames))
 {
-    $Commands = Get-Command -Module:($ModuleNames)
+    $Commands = Get-Command -Module:($ModuleNames) # -Verb:('Get') -Noun:('JcSdkApplication') # Use to troubleshoot single command
     ForEach ($Command In $Commands)
     {
         # Create new function name
         $ModuleName = $Command.Module.Name
         $CommandName = $Command.Name
         $NewFunctionName = $CommandName.Replace($ModulePrefix, 'JC')
-        # Build new function parameters
-        $CommandParameterSets = $Command.ParameterSets
-        $CommandParameters = $Command.Parameters
-        $CommandDefinition = $Command.Definition
-        # Get the CmdletBinding and OutputType
-        $OutputTypeMatch = $CommandDefinition | Select-String -Pattern:('(\[OutputType)(.*?)(\]\r)')
-        $OutputType = If ($OutputTypeMatch.Matches.Value) { $OutputTypeMatch.Matches.Value } Else { '' }
-        $CmdletBindingMatch = $CommandDefinition | Select-String -Pattern:('(\[CmdletBinding)(.*?)(\]\r)')
-        $CmdletBinding = If ($CmdletBindingMatch.Matches.Value) { $CmdletBindingMatch.Matches.Value } Else { '' }
-        If (-not [System.String]::IsNullOrEmpty($OutputType)) { $CmdletBinding = "$($OutputType)`n$($IndentChar)$($CmdletBinding)" }
         # Get content from sdk function
         $CommandFilePath = $Command.ScriptBlock.File
-        $CommandContent = Get-Content -Path:($CommandFilePath) -Raw
-        $PSScriptInfo = ($CommandContent | Select-String -Pattern:('(?s)(<#)(.*?)(#>)')).Matches.Value
-        $Params = $CommandContent | Select-String -Pattern:('(?s)(    \[Parameter)(.*?)(\})') -AllMatches
-        $NewParameters = ($Params.Matches.Value | Where-Object { $_ -notlike '*DontShow*' }) -join ",`n`n"
-        # Hacky
+        $CommandFilePathContent = Get-Content -Path:($CommandFilePath) -Raw
+        $FunctionContent = If ($CommandFilePath -like '*ProxyCmdletDefinitions.ps1')
+        {
+            <# When the autorest generated module has been installed and imported from the PSGallery all the
+            cmdlets will exist in a single ProxyCmdletDefinitions.ps1 file. We need to parse
+            out the specific function in order to gather the parts we need to copy over. #>
+            $CommandFilePathContent.Replace($MSCopyrightHeader, $Divider).Split($Divider).Where( { $_ -like ('*' + "function $CommandName {" + '*') })
+        }
+        Else
+        {
+            <# When the autorest generated module has been imported from a local psd1 module the function will
+            remain in their individual files. #>
+            $CommandFilePathContent
+        }
+        # Extract the sections we want to copy over to our new function.
+        $Params = $FunctionContent | Select-String -Pattern:('(?s)(    \[Parameter)(.*?)(\})') -AllMatches
+        $PSScriptInfo = ($FunctionContent | Select-String -Pattern:('(?s)(<#)(.*?)(#>)')).Matches.Value
+        $OutputType = ($FunctionContent | Select-String -Pattern:('(\[OutputType)(.*?)(\]\r)')).Matches.Value
+        $CmdletBinding = ($FunctionContent | Select-String -Pattern:('(\[CmdletBinding)(.*?)(\]\r)')).Matches.Value
+        # Filter/Manipulate extracted content
+        $ParameterContent = ($Params.Matches.Value | Where-Object { $_ -notlike '*DontShow*' }) -join ",`n`n"
         $PSScriptInfo = $PSScriptInfo.Replace(($PSScriptInfo | Select-String -Pattern:('(?s)(\.Link)(.*?)(docs.microsoft.com)(.*?)(\n)')).Matches.Value, '')
         $PSScriptInfo = $PSScriptInfo.Replace($ModulePrefix, 'JC')
+        If (-not [System.String]::IsNullOrEmpty($OutputType)) { $CmdletBinding = "$($OutputType)`n$($IndentChar)$($CmdletBinding)" }
+        # # Build new function parameters
+        # $CommandParameterSets = $Command.ParameterSets
+        # $CommandParameters = $Command.Parameters
+        # $CommandDefinition = $Command.Definition
         # # Build parameters by copying them from the SDK function parameters
-        # $NewParameters = ForEach ($CommandParameter In $CommandParameters.GetEnumerator())
+        # $ParameterContent = ForEach ($CommandParameter In $CommandParameters.GetEnumerator())
         # {
         #     # $ParameterName = $CommandParameter.Key
         #     # $ParameterValue.IsDynamic
@@ -138,11 +149,11 @@ If (Get-Module -Name($ModuleNames))
         #         }
         #     }
         # }
-        # $NewParameters = ($NewParameters -join ",`n")
+        # $ParameterContent = ($ParameterContent -join ",`n")
         # Build $BeginContent, $ProcessContent, and $EndContent
         If ($Command.Verb -in ('Get', 'Search'))
         {
-            If (-not [System.String]::IsNullOrEmpty($NewParameters)) { $NewParameters = $NewParameters + ",`n`n$($IndentChar)[System.Boolean]`n$($IndentChar)# Set to `$true to return all results.`n$($IndentChar)`$Paginate = `$true" }
+            If (-not [System.String]::IsNullOrEmpty($ParameterContent)) { $ParameterContent = $ParameterContent + ",`n`n$($IndentChar)[System.Boolean]`n$($IndentChar)# Set to `$true to return all results.`n$($IndentChar)`$Paginate = `$true" }
             # Build script content
             If ($ModuleName -eq 'JumpCloud.SDK.DirectoryInsights')
             {
@@ -286,7 +297,7 @@ $($IndentChar)$($IndentChar)}"
         If (-not [System.String]::IsNullOrEmpty($BeginContent) -and -not [System.String]::IsNullOrEmpty($ProcessContent) -and -not [System.String]::IsNullOrEmpty($EndContent))
         {
             # Build "Function"
-            $NewScript = $FunctionTemplate -f $PSScriptInfo, $NewFunctionName, $ModuleName, $CmdletBinding, $NewParameters, $BeginContent, $ProcessContent, $EndContent
+            $NewScript = $FunctionTemplate -f $PSScriptInfo, $NewFunctionName, $ModuleName, $CmdletBinding, $ParameterContent, $BeginContent, $ProcessContent, $EndContent
             # Fix line endings
             $NewScript = $NewScript.Replace("`r`n", "`n").Trim()
             # Export the function
