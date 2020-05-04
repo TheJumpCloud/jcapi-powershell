33c6b2e1dc2e248c1269673b5966553f6d615f09
Microsoft.VisualStudio.Services.TFS <VssAdministrator@FakeEmail.com>
2020-05-04 12:09:29 +0000

    Updating module: JumpCloud.SDK.V2;[skip ci]

--- AutoRest/SDKs/PowerShell/JumpCloud.SDK.V2/custom/Module.cs
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V2/custom/Module.cs
@@ -3,10 +3,10 @@ namespace JumpCloud.SDK.V2
     using Runtime;
     using System;
     using System.Collections.Generic;
+    using System.Management.Automation;
     using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Management.Automation;
     /// <summary>A class that contains the module-common code and data.</summary>
     /// <notes>
     /// This class is where you can add things to modify the module.
@@ -47,34 +47,32 @@ namespace JumpCloud.SDK.V2
                 System.Environment.SetEnvironmentVariable("JCOrgId", JCOrgId);
                 request.Headers.Add("x-org-id", JCOrgId);
             }
+            // If headers do not contain an "x-api-key" header add one
             if (request.Headers.Contains("x-api-key") == false)
             {
                 request.Headers.Add("x-api-key", JCApiKey);
             }
+            // If headers do not contain an "Accept" header add one
             if (request.Headers.Contains("Accept") == false)
             {
                 request.Headers.Add("Accept", "application/json");
             }
-            request.Headers.UserAgent.ParseAdd("JumpCloud_JumpCloud.PowerShell.SDK.V2/0.0.13");
+            // If headers do not contain an "UserAgent" with the correct value fix it
+            if (request.Headers.UserAgent.ToString() != "JumpCloud_JumpCloud.PowerShell.SDK.V2/0.0.1")
+            {
+                request.Headers.UserAgent.Clear();
+                request.Headers.UserAgent.ParseAdd("JumpCloud_JumpCloud.PowerShell.SDK.V2/0.0.1");
+            }
             // request.Headers.Add("Content-Type", "application/json");
             // let it go on.
             var requestResult = await next.SendAsync(request, callback);
-            // Console.WriteLine("------------------------------------------------------------------------");
-            // Console.WriteLine("RequestUri : {0}", request.RequestUri);
-            // Console.WriteLine("Content: {0}", requestResult.Content);
-            // Console.WriteLine(requestResult.Content.ToString());
-            // Console.WriteLine("Headers: {0}", requestResult.Headers);
-            // Console.WriteLine("RequestMessage: {0}", requestResult.RequestMessage);
-            // Console.WriteLine("IsSuccessStatusCode: {0}", requestResult.IsSuccessStatusCode);
-            // Console.WriteLine("ReasonPhrase: {0}", requestResult.ReasonPhrase);
-            // Console.WriteLine("StatusCode: {0}", requestResult.StatusCode);
-            // Console.WriteLine("Version: {0}", requestResult.Version);
             if (requestResult.IsSuccessStatusCode)
             {
                 return requestResult;
             }
             else
             {
+                var ResponseContent = await requestResult.Content.ReadAsStringAsync();
                 if (requestResult.ReasonPhrase == "Unauthorized")
                 {
                     System.Environment.SetEnvironmentVariable("JCApiKey", "");
@@ -89,9 +87,27 @@ namespace JumpCloud.SDK.V2
                 + requestResult.ReasonPhrase
                 + Environment.NewLine
                 + requestResult.RequestMessage
-                + ";"
+                + Environment.NewLine
+                + " RequestContent: " + request.Content.ReadAsStringAsync().Result
+                + Environment.NewLine
+                + " ResponseContent: " + ResponseContent
                  );
             }
+            // Console.WriteLine("------------------------------------------------------------------------");
+            // Console.WriteLine("RequestBody: '{0}'", request.Content.ReadAsStringAsync().Result);
+            // Console.WriteLine("------------------------------------------------------------------------");
+            // Console.WriteLine("------------------------------------------------------------------------");
+            // Console.WriteLine("StackTrace: '{0}'", Environment.StackTrace);
+            // Console.WriteLine("ResponseContent: {0}", ResponseContent);
+            // Console.WriteLine("------------------------------------------------------------------------");
+            // Console.WriteLine("RequestUri : {0}", request.RequestUri);
+            // Console.WriteLine(requestResult.Content.ReadAsStringAsync());
+            // Console.WriteLine("Headers: {0}", requestResult.Headers);
+            // Console.WriteLine("RequestMessage: {0}", requestResult.RequestMessage);
+            // Console.WriteLine("IsSuccessStatusCode: {0}", requestResult.IsSuccessStatusCode);
+            // Console.WriteLine("ReasonPhrase: {0}", requestResult.ReasonPhrase);
+            // Console.WriteLine("StatusCode: {0}", requestResult.StatusCode);
+            // Console.WriteLine("Version: {0}", requestResult.Version);
         }
     }
 }
