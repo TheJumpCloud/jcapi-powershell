5dbfc883d5b66ae06d85e34c4eaea6d5a9858164
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-05-15 08:45:59 -0600

    rebuild

--- AutoRest/Custom/Module.cs
+++ AutoRest/Custom/Module.cs
@@ -3,10 +3,10 @@ namespace ModuleNameSpace
     using Runtime;
     using System;
     using System.Collections.Generic;
-    using System.Management.Automation;
     using System.Net.Http;
-    using System.Threading;
     using System.Threading.Tasks;
+    using System.Linq;
+    // using System.Management.Automation;
     /// <summary>A class that contains the module-common code and data.</summary>
     /// <notes>
     /// This class is where you can add things to modify the module.
@@ -17,17 +17,28 @@ namespace ModuleNameSpace
     {
         partial void CustomInit()
         {
-            // we need to add a step at the end of the pipeline
-            // to attach the API key
-            // once for the regular pipeline
-            this._pipeline.Append(AddAuth);
-            // once for the pipeline that supports a proxy
-            this._pipelineWithProxy.Append(AddAuth);
+            // We need to add a steps to the pipeline
+            // Add Headers
+            this._pipeline.Prepend(AddAuthHeaders);
+            this._pipelineWithProxy.Prepend(AddAuthHeaders);
+            // Add CustomErrors
+            this._pipeline.Prepend(CustomError);
+            this._pipelineWithProxy.Prepend(CustomError);
+            // // Add Debugging Messages
+            // this._pipeline.Prepend(Debugging);
+            // this._pipelineWithProxy.Prepend(Debugging);
+            // // Add Paginate
+            // this._pipeline.Append(Paginate);
+            // this._pipelineWithProxy.Append(Paginate);
         }
-        protected async Task<HttpResponseMessage> AddAuth(HttpRequestMessage request, IEventListener callback, ISendAsync next)
+        partial void AfterCreatePipeline(global::System.Management.Automation.InvocationInfo invocationInfo, ref JumpCloud.SDK.DirectoryInsights.Runtime.HttpPipeline pipeline)
         {
+            pipeline.Append(Paginate);
+        }
+        protected async Task<HttpResponseMessage> AddAuthHeaders(HttpRequestMessage request, IEventListener callback, ISendAsync next)
+        {
+            // Check to see if the environment variable for JCApiKey is populated
             var JCApiKey = System.Environment.GetEnvironmentVariable("JCApiKey");
-            var JCOrgId = System.Environment.GetEnvironmentVariable("JCOrgId");
             if (JCApiKey == null || JCApiKey == "")
             {
                 Console.Write("Please enter your JumpCloud API key: ");
@@ -35,22 +46,29 @@ namespace ModuleNameSpace
                 Console.WriteLine("You entered '{0}'", JCApiKey);
                 System.Environment.SetEnvironmentVariable("JCApiKey", JCApiKey);
             }
-            if (request.RequestUri.ToString() == "https://console.jumpcloud.com/api/organizations")
+            // If headers do not contain an "x-api-key" header add one
+            if (request.Headers.Contains("x-api-key") == false)
             {
-                System.Environment.SetEnvironmentVariable("JCOrgId", "");
+                request.Headers.Add("x-api-key", JCApiKey);
             }
-            else if (JCOrgId == null || JCOrgId == "")
+            // Check to see if the environment variable for JCOrgId is populated
+            var JCOrgId = System.Environment.GetEnvironmentVariable("JCOrgId");
+            if (JCOrgId == null || JCOrgId == "")
             {
                 Console.Write("Please enter your JumpCloud organization id: ");
                 JCOrgId = Console.ReadLine();
                 Console.WriteLine("You entered '{0}'", JCOrgId);
                 System.Environment.SetEnvironmentVariable("JCOrgId", JCOrgId);
+            }
+            // If headers do not contain an "x-org-id" header add one
+            if (request.Headers.Contains("x-org-id") == false)
+            {
                 request.Headers.Add("x-org-id", JCOrgId);
             }
-            // If headers do not contain an "x-api-key" header add one
-            if (request.Headers.Contains("x-api-key") == false)
+            // Organization endpoint does not accept x-org-id as a header so remove it
+            if (request.Headers.Contains("x-org-id") && request.RequestUri.ToString() == "https://console.jumpcloud.com/api/organizations")
             {
-                request.Headers.Add("x-api-key", JCApiKey);
+                request.Headers.Remove("x-org-id");
             }
             // If headers do not contain an "Accept" header add one
             if (request.Headers.Contains("Accept") == false)
@@ -63,88 +81,107 @@ namespace ModuleNameSpace
                 request.Headers.UserAgent.Clear();
                 request.Headers.UserAgent.ParseAdd("JumpCloud_ModuleNameSpace/ModuleVersion");
             }
-            // request.Headers.Add("Content-Type", "application/json");
+            // // request.Headers.Add("Content-Type", "application/json");
+            System.Net.Http.HttpResponseMessage response = await next.SendAsync(request, callback);
+            return response;
+        }
+        protected async Task<HttpResponseMessage> Debugging(HttpRequestMessage request, IEventListener callback, ISendAsync next)
+        {
+            System.Net.Http.HttpResponseMessage response = await next.SendAsync(request, callback);
             Console.WriteLine("------------------------------------------------------------------------");
             Console.WriteLine("Request: '{0}'", request);
             Console.WriteLine("------------------------------------------------------------------------");
             Console.WriteLine("RequestBody: '{0}'", request.Content.ReadAsStringAsync().Result);
             Console.WriteLine("------------------------------------------------------------------------");
-            // let it go on.
-            System.Net.Http.HttpResponseMessage result = null;
-            var XResultSearchAfter;
-            var XLimit;
-            // System.Collections.ArrayList results = new ArrayList();
-            while (true)
-            {
-                // make the call
-                result = await next.SendAsync(request, callback);
-                // results.Add(result);
-                // check to see if we want to retry
-
-                result.Result.Headers.TryGetValues("X-Search_after", XResultSearchAfter);
-                result.Result.Headers.TryGetValues("X-Limit", XLimit);
-                Console.WriteLine(XResultSearchAfter);
-                Console.WriteLine(XLimit);
-                // // While ($XResultCount -eq $XLimit -and $Result)
-
-                // if (result && count-- == XLimit)
-                // {
-                //     // wait before retrying
-                //     await System.Threading.Tasks.Task.Delay(10000);
-                //     continue;
-                // }
-                // else
-                // {
-                //     // apparently not retrying, let's get out.
-                break;
-                // }
-            };
-            // return whatever we have.
-            return result;
-
-            // var result = await next.SendAsync(request, callback);
-            // if (result.IsSuccessStatusCode)
-            // {
-            //     return result;
-            // }
-            // else
-            // {
-            //     var ResponseContent = await result.Content.ReadAsStringAsync();
-            //     if (result.ReasonPhrase == "Unauthorized")
-            //     {
-            //         System.Environment.SetEnvironmentVariable("JCApiKey", "");
-            //         System.Environment.SetEnvironmentVariable("JCOrgId", "");
-            //     }
-            //     throw new Exception(
-            //     "JumpCloudApiSdkError:: "
-            //     + Environment.NewLine
-            //     + " StatusCode: "
-            //     + (int)result.StatusCode
-            //     + " - "
-            //     + result.ReasonPhrase
-            //     + Environment.NewLine
-            //     + result.RequestMessage
-            //     + Environment.NewLine
-            //     + " RequestContent: " + request.Content.ReadAsStringAsync().Result
-            //     + Environment.NewLine
-            //     + " ResponseContent: " + ResponseContent
-            //      );
-            // }
-            // Console.WriteLine("------------------------------------------------------------------------");
-            // Console.WriteLine("RequestBody: '{0}'", request.Content.ReadAsStringAsync().Result);
-            // Console.WriteLine("------------------------------------------------------------------------");
             // Console.WriteLine("------------------------------------------------------------------------");
             // Console.WriteLine("StackTrace: '{0}'", Environment.StackTrace);
             // Console.WriteLine("ResponseContent: {0}", ResponseContent);
             // Console.WriteLine("------------------------------------------------------------------------");
             // Console.WriteLine("RequestUri : {0}", request.RequestUri);
-            // Console.WriteLine(result.Content.ReadAsStringAsync());
-            // Console.WriteLine("Headers: {0}", result.Headers);
-            // Console.WriteLine("RequestMessage: {0}", result.RequestMessage);
-            // Console.WriteLine("IsSuccessStatusCode: {0}", result.IsSuccessStatusCode);
-            // Console.WriteLine("ReasonPhrase: {0}", result.ReasonPhrase);
-            // Console.WriteLine("StatusCode: {0}", result.StatusCode);
-            // Console.WriteLine("Version: {0}", result.Version);
+            // Console.WriteLine(response.Content.ReadAsStringAsync());
+            // Console.WriteLine("Headers: {0}", response.Headers);
+            // Console.WriteLine("RequestMessage: {0}", response.RequestMessage);
+            // Console.WriteLine("IsSuccessStatusCode: {0}", response.IsSuccessStatusCode);
+            // Console.WriteLine("ReasonPhrase: {0}", response.ReasonPhrase);
+            // Console.WriteLine("StatusCode: {0}", response.StatusCode);
+            // Console.WriteLine("Version: {0}", response.Version);
+            return response;
+        }
+        protected async Task<HttpResponseMessage> CustomError(HttpRequestMessage request, IEventListener callback, ISendAsync next)
+        {
+            System.Net.Http.HttpResponseMessage response = await next.SendAsync(request, callback);
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+            else
+            {
+                var ResponseContent = await response.Content.ReadAsStringAsync();
+                if (response.ReasonPhrase == "Unauthorized")
+                {
+                    System.Environment.SetEnvironmentVariable("JCApiKey", "");
+                    System.Environment.SetEnvironmentVariable("JCOrgId", "");
+                }
+                throw new Exception(
+                "JumpCloudApiSdkError:: "
+                + Environment.NewLine
+                + " StatusCode: "
+                + (int)response.StatusCode
+                + " - "
+                + response.ReasonPhrase
+                + Environment.NewLine
+                + response.RequestMessage
+                + Environment.NewLine
+                + " RequestContent: " + request.Content.ReadAsStringAsync().Result
+                + Environment.NewLine
+                + " ResponseContent: " + ResponseContent
+                 );
+            }
+        }
+        // protected async Task<HttpResponseMessage> Paginate(HttpRequestMessage request, IEventListener callback, ISendAsync next)
+        public async System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage> Paginate(System.Net.Http.HttpRequestMessage requestMessage, Runtime.IEventListener listener, Runtime.ISendAsync next)
+        {
+            System.Net.Http.HttpResponseMessage response = null;
+            while (true)
+            {
+                // Make the API call
+                response = await next.SendAsync(requestMessage, listener);
+                // Get ResultCount
+                IEnumerable<string> XResultCount;
+                response.Headers.TryGetValues("X-Result-Count", out XResultCount);
+                var XResultCountString = XResultCount.ToList()[0];
+                // Get Limit
+                IEnumerable<string> XLimit;
+                response.Headers.TryGetValues("X-Limit", out XLimit);
+                var XLimitString = XLimit.ToList()[0];
+                // Get SearchAfter
+                IEnumerable<string> XResultSearchAfter;
+                response.Headers.TryGetValues("X-Search_after", out XResultSearchAfter);
+                var XResultSearchAfterString = XResultSearchAfter.ToList()[0];
+                // Write to host
+                Console.WriteLine("XResultCount: " + XResultCountString);
+                Console.WriteLine("XLimit: " + XLimitString);
+                Console.WriteLine("XResultSearchAfter: " + XResultSearchAfterString);
+                // Modify headers with new XResultSearchAfter
+                if (XResultCountString == XLimitString) // && response == true)
+                {
+                    ////////////////////////////////////////////////////////////////////////////////////
+                    // request.Content.Remove("X-Search_after");
+                    // request.Content.Add("X-Search_after", XResultSearchAfterString);
+                    // .Content = new StringContent("{\"name\":\"John Doe\",\"age\":33}", Encoding.UTF8, "application/json");
+                    ////////////////////////////////////////////////////////////////////////////////////
+                    // wait before getting more results
+                    await System.Threading.Tasks.Task.Delay(5000);
+                    continue;
+                }
+                else
+                {
+                    // no more results, break loop.
+                    break;
+                }
+            };
+            // return whatever we have.
+            return response;
         }
     }
 }
