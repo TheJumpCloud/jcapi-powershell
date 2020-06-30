c24d1a345421d80dbb3cc9fd399ddf20b4cf0436
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-06-29 19:05:20 -0600

    updated v1

---
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/custom/Get-JCSystem.ps1
@@ -0,0 +1,141 @@
+<#
+.Synopsis
+This endpoint returns an individual system.\n\n#### Sample Request\n```\ncurl -X GET https://console.jumpcloud.com/api/systems/{SystemID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \n  ```
+.Description
+This endpoint returns an individual system.\n\n#### Sample Request\n```\ncurl -X GET https://console.jumpcloud.com/api/systems/{SystemID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \n  ```
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity
+.Outputs
+JumpCloud.SDK.V1.Models.IJcSystem
+.Outputs
+JumpCloud.SDK.V1.Models.ISystemslist
+.Notes
+COMPLEX PARAMETER PROPERTIES
+
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+INPUTOBJECT <IJumpCloudApIsIdentity>: 
+  [Id <String>]: 
+  [SystemuserId <String>]: 
+  [Triggername <String>]: 
+.Link
+https://github.com/TheJumpCloud/jcapi-powershell/tree/master/AutoRest/SDKs/PowerShell/jumpcloud.sdk.v1/get-jcsystem
+#>
+ Function Get-JCSystem
+{
+    [OutputType([JumpCloud.SDK.V1.Models.IJcSystem], [JumpCloud.SDK.V1.Models.ISystemslist])]
+    [CmdletBinding(DefaultParameterSetName='List', PositionalBinding=$false)]
+    Param(
+    [Parameter(ParameterSetName='Get', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [System.String]
+    # .
+    ${Id},
+
+    [Parameter(ParameterSetName='GetViaIdentity', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity]
+    # Identity Parameter
+    # To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
+    ${InputObject},
+
+    [Parameter()]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.String]
+    # Use a space seperated string of field parameters to include the data in the response.
+    # If omitted, the default list of fields will be returned.
+    ${Fields},
+
+    [Parameter()]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.String]
+    # A filter to apply to the query.
+    ${Filter},
+
+    [Parameter(ParameterSetName='List')]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.Int32]
+    # The number of records to return at once.
+    # Limited to 100.
+    ${Limit},
+
+    [Parameter(ParameterSetName='List')]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.String]
+    # A nested object containing a string `searchTerm` and a list of `fields` to search on.
+    ${Search},
+
+    [Parameter(ParameterSetName='List')]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.Int32]
+    # The offset into the records to return.
+    ${Skip},
+
+    [Parameter(ParameterSetName='List')]
+    [JumpCloud.SDK.V1.Category('Query')]
+    [System.String]
+    # Use space separated sort parameters to sort the collection.
+    # Default sort is ascending.
+    # Prefix with `-` to sort descending.
+    ${Sort},
+
+    [Parameter(DontShow)]
+    [System.Boolean]
+    # Set to $true to return all results. This will overwrite any skip and limit parameter.
+    $Paginate = $true
+    )
+    Begin
+    {
+        $Results = @()
+        If ([System.String]::IsNullOrEmpty($PSBoundParameters.Skip))
+        {
+            $PSBoundParameters.Add('Skip', 0)
+        }
+        If ([System.String]::IsNullOrEmpty($PSBoundParameters.Limit))
+        {
+            $PSBoundParameters.Add('Limit', 100)
+        }
+    }
+    Process
+    {
+        If ($Paginate)
+        {
+            $PSBoundParameters.Remove('Paginate') | Out-Null
+            Do
+            {
+                Write-Debug ("Skip: $($PSBoundParameters.Skip); Limit: $($PSBoundParameters.Limit);");
+                $Result = JumpCloud.SDK.V1.internal\Get-JcSdkSystem @PSBoundParameters
+                If (-not [System.String]::IsNullOrEmpty($Result))
+                {
+                    $ResultCount = ($Result.results | Measure-Object).Count;
+                    $Results += $Result.results;
+                    $PSBoundParameters.Skip += $ResultCount
+                }
+            }
+            While ($ResultCount -eq $PSBoundParameters.Limit -and $Result)
+        }
+        Else
+        {
+            $PSBoundParameters.Remove('Paginate') | Out-Null
+            $Result = JumpCloud.SDK.V1.internal\Get-JcSdkSystem @PSBoundParameters
+            If (-not [System.String]::IsNullOrEmpty($Result))
+            {
+                $Results += $Result.results;
+            }
+        }
+    }
+    End
+    {
+        Return $Results
+    }
+}
+
---
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/custom/Remove-JCSystemUser.ps1
@@ -0,0 +1,68 @@
+<#
+.Synopsis
+This endpoint allows you to delete a particular system user.\n\n#### Sample Request\n```\ncurl -X DELETE https://console.jumpcloud.com/api/systemusers/{UserID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \n```
+.Description
+This endpoint allows you to delete a particular system user.\n\n#### Sample Request\n```\ncurl -X DELETE https://console.jumpcloud.com/api/systemusers/{UserID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \n```
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity
+.Outputs
+JumpCloud.SDK.V1.Models.ISystemuserreturn
+.Notes
+COMPLEX PARAMETER PROPERTIES
+
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+INPUTOBJECT <IJumpCloudApIsIdentity>: 
+  [Id <String>]: 
+  [SystemuserId <String>]: 
+  [Triggername <String>]: 
+.Link
+https://github.com/TheJumpCloud/jcapi-powershell/tree/master/AutoRest/SDKs/PowerShell/jumpcloud.sdk.v1/remove-jcsystemuser
+#>
+ Function Remove-JCSystemUser
+{
+    [OutputType([JumpCloud.SDK.V1.Models.ISystemuserreturn])]
+    [CmdletBinding(DefaultParameterSetName='Delete', PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+    [Parameter(ParameterSetName='Delete', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [System.String]
+    # .
+    ${Id},
+
+    [Parameter(ParameterSetName='DeleteViaIdentity', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity]
+    # Identity Parameter
+    # To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
+    ${InputObject},
+
+    [Parameter()]
+    [JumpCloud.SDK.V1.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Returns true when the command succeeds
+    ${PassThru}
+    )
+    Begin
+    {
+        $Results = @()
+    }
+    Process
+    {
+        $Results = JumpCloud.SDK.V1.internal\Remove-JcSdkSystemUser @PSBoundParameters
+    }
+    End
+    {
+        Return $Results
+    }
+}
+
