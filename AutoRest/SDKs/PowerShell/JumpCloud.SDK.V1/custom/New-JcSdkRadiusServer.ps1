a87c8cf534e0ceeacd183cd1aedbdf421e8ef469
Microsoft.VisualStudio.Services.TFS <VssAdministrator@FakeEmail.com>
2020-07-01 12:06:43 +0000

    Updating module: JumpCloud.SDK.V1;[skip ci]

---
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/custom/New-JcSdkRadiusServer.ps1
@@ -0,0 +1,102 @@
+<#
+.Synopsis
+This endpoint allows you to create RADIUS servers in your organization.\n\n#### Sample Request\n```\ncurl -X POST https://console.jumpcloud.com/api/radiusservers/ \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \\\n  -d '{\n    \"name\": \"{test_radius}\",\n    \"networkSourceIp\": \"{0.0.0.0}\",\n    \"sharedSecret\":\"{secretpassword}\",\n    \"userLockoutAction\": \"REMOVE\",\n    \"userPasswordExpirationAction\": \"MAINTAIN\"\n}'\n```
+.Description
+This endpoint allows you to create RADIUS servers in your organization.\n\n#### Sample Request\n```\ncurl -X POST https://console.jumpcloud.com/api/radiusservers/ \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \\\n  -d '{\n    \"name\": \"{test_radius}\",\n    \"networkSourceIp\": \"{0.0.0.0}\",\n    \"sharedSecret\":\"{secretpassword}\",\n    \"userLockoutAction\": \"REMOVE\",\n    \"userPasswordExpirationAction\": \"MAINTAIN\"\n}'\n```
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+JumpCloud.SDK.V1.Models.IRadiusserverpost
+.Outputs
+JumpCloud.SDK.V1.Models.IRadiusserver
+.Notes
+COMPLEX PARAMETER PROPERTIES
+
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+BODY <IRadiusserverpost>: 
+  Name <String>: 
+  NetworkSourceIP <String>: 
+  SharedSecret <String>: RADIUS shared secret between the server and client.
+  [Mfa <String>]: 
+  [TagNames <String[]>]: 
+  [UserLockoutAction <String>]: 
+  [UserPasswordExpirationAction <String>]: 
+.Link
+https://github.com/TheJumpCloud/jcapi-powershell/tree/master/AutoRest/SDKs/PowerShell/jumpcloud.sdk.v1/new-jcsdkradiusserver
+#>
+ Function New-JcSdkRadiusServer
+{
+    [OutputType([JumpCloud.SDK.V1.Models.IRadiusserver])]
+    [CmdletBinding(DefaultParameterSetName='CreateExpanded', PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+    [Parameter(ParameterSetName='Create', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.IRadiusserverpost]
+    # RadiusServerPost
+    # To construct, see NOTES section for BODY properties and create a hash table.
+    ${Body},
+
+    [Parameter(ParameterSetName='CreateExpanded', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Name},
+
+    [Parameter(ParameterSetName='CreateExpanded', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${NetworkSourceIP},
+
+    [Parameter(ParameterSetName='CreateExpanded', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # RADIUS shared secret between the server and client.
+    ${SharedSecret},
+
+    [Parameter(ParameterSetName='CreateExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Mfa},
+
+    [Parameter(ParameterSetName='CreateExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String[]]
+    # .
+    ${TagNames},
+
+    [Parameter(ParameterSetName='CreateExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${UserLockoutAction},
+
+    [Parameter(ParameterSetName='CreateExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${UserPasswordExpirationAction}
+    )
+    Begin
+    {
+        $Results = @()
+    }
+    Process
+    {
+        $Results = JumpCloud.SDK.V1.internal\New-JcSdkInternalRadiusServer @PSBoundParameters
+    }
+    End
+    {
+        Return $Results
+    }
+}
+
---
+++ AutoRest/SDKs/PowerShell/JumpCloud.SDK.V1/custom/Set-JcSdkSystemUser.ps1
@@ -0,0 +1,435 @@
+<#
+.Synopsis
+This endpoint allows you to update a system user.\n\n#### Sample Request\n\n```\ncurl -X PUT https://console.jumpcloud.com/api/systemusers/{UserID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \\\n  -d '{\n\t\"email\":\"{email_address}\",\n\t\"firstname\":\"{Name}\",\n\t\"lastname\":\"{Name}\"\n}'\n```
+.Description
+This endpoint allows you to update a system user.\n\n#### Sample Request\n\n```\ncurl -X PUT https://console.jumpcloud.com/api/systemusers/{UserID} \\\n  -H 'Accept: application/json' \\\n  -H 'Content-Type: application/json' \\\n  -H 'x-api-key: {API_KEY}' \\\n  -d '{\n\t\"email\":\"{email_address}\",\n\t\"firstname\":\"{Name}\",\n\t\"lastname\":\"{Name}\"\n}'\n```
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity
+.Inputs
+JumpCloud.SDK.V1.Models.ISystemuserput
+.Outputs
+JumpCloud.SDK.V1.Models.ISystemuserreturn
+.Notes
+COMPLEX PARAMETER PROPERTIES
+
+To create the parameters described below, construct a hash table containing the appropriate properties. For information on hash tables, run Get-Help about_Hash_Tables.
+
+ADDRESSES <ISystemuserputAddressesItem[]>: 
+  [Country <String>]: 
+  [ExtendedAddress <String>]: 
+  [Locality <String>]: 
+  [PoBox <String>]: 
+  [PostalCode <String>]: 
+  [Region <String>]: 
+  [StreetAddress <String>]: 
+  [Type <String>]: 
+
+BODY <ISystemuserput>: 
+  [AccountLocked <Boolean?>]: 
+  [Addresses <ISystemuserputAddressesItem[]>]: type, poBox, extendedAddress, streetAddress, locality, region, postalCode, country
+    [Country <String>]: 
+    [ExtendedAddress <String>]: 
+    [Locality <String>]: 
+    [PoBox <String>]: 
+    [PostalCode <String>]: 
+    [Region <String>]: 
+    [StreetAddress <String>]: 
+    [Type <String>]: 
+  [AllowPublicKey <Boolean?>]: 
+  [Attributes <ISystemuserputAttributesItem[]>]: 
+  [Company <String>]: 
+  [CostCenter <String>]: 
+  [Department <String>]: 
+  [Description <String>]: 
+  [Displayname <String>]: 
+  [Email <String>]: 
+  [EmployeeIdentifier <String>]: Must be unique per user. 
+  [EmployeeType <String>]: 
+  [EnableManagedUid <Boolean?>]: 
+  [EnableUserPortalMultifactor <Boolean?>]: 
+  [ExternalDn <String>]: 
+  [ExternalPasswordExpirationDate <String>]: 
+  [ExternalSourceType <String>]: 
+  [ExternallyManaged <Boolean?>]: 
+  [Firstname <String>]: 
+  [JobTitle <String>]: 
+  [Lastname <String>]: 
+  [LdapBindingUser <Boolean?>]: 
+  [Location <String>]: 
+  [MfaConfigured <Boolean?>]: 
+  [MfaExclusion <Boolean?>]: 
+  [MfaExclusionUntil <DateTime?>]: 
+  [Middlename <String>]: 
+  [Password <String>]: 
+  [PasswordNeverExpires <Boolean?>]: 
+  [PhoneNumbers <ISystemuserputPhoneNumbersItem[]>]: 
+    [Number <String>]: 
+    [Type <String>]: 
+  [PublicKey <String>]: 
+  [Relationships <ISystemuserputRelationshipsItem[]>]: 
+  [SambaServiceUser <Boolean?>]: 
+  [SshKeys <ISshkeypost[]>]: 
+    Name <String>: The name of the SSH key.
+    PublicKey <String>: The Public SSH key.
+  [Sudo <Boolean?>]: 
+  [Suspended <Boolean?>]: 
+  [Tags <String[]>]: 
+  [UnixGuid <Int32?>]: 
+  [UnixUid <Int32?>]: 
+  [Username <String>]: 
+
+INPUTOBJECT <IJumpCloudApIsIdentity>: 
+  [Id <String>]: 
+  [SystemuserId <String>]: 
+  [Triggername <String>]: 
+
+PHONENUMBERS <ISystemuserputPhoneNumbersItem[]>: 
+  [Number <String>]: 
+  [Type <String>]: 
+
+SSHKEYS <ISshkeypost[]>: 
+  Name <String>: The name of the SSH key.
+  PublicKey <String>: The Public SSH key.
+.Link
+https://github.com/TheJumpCloud/jcapi-powershell/tree/master/AutoRest/SDKs/PowerShell/jumpcloud.sdk.v1/set-jcsdksystemuser
+#>
+ Function Set-JcSdkSystemUser
+{
+    [OutputType([JumpCloud.SDK.V1.Models.ISystemuserreturn])]
+    [CmdletBinding(DefaultParameterSetName='PutExpanded', PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
+    Param(
+    [Parameter(ParameterSetName='Put', Mandatory)]
+    [Parameter(ParameterSetName='PutExpanded', Mandatory)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [System.String]
+    # .
+    ${Id},
+
+    [Parameter(ParameterSetName='PutViaIdentity', Mandatory, ValueFromPipeline)]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.V1.Category('Path')]
+    [JumpCloud.SDK.V1.Models.IJumpCloudApIsIdentity]
+    # Identity Parameter
+    # To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
+    ${InputObject},
+
+    [Parameter(ParameterSetName='Put', Mandatory, ValueFromPipeline)]
+    [Parameter(ParameterSetName='PutViaIdentity', Mandatory, ValueFromPipeline)]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISystemuserput]
+    # SystemUserPut
+    # To construct, see NOTES section for BODY properties and create a hash table.
+    ${Body},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${AccountLocked},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISystemuserputAddressesItem[]]
+    # type, poBox, extendedAddress, streetAddress, locality, region, postalCode, country
+    # To construct, see NOTES section for ADDRESSES properties and create a hash table.
+    ${Addresses},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${AllowPublicKey},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISystemuserputAttributesItem[]]
+    # .
+    ${Attributes},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Company},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${CostCenter},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Department},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Description},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Displayname},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Email},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # Must be unique per user.
+    ${EmployeeIdentifier},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${EmployeeType},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${EnableManagedUid},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${EnableUserPortalMultifactor},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${ExternalDn},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${ExternalPasswordExpirationDate},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${ExternalSourceType},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${ExternallyManaged},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Firstname},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${JobTitle},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Lastname},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${LdapBindingUser},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Location},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${MfaConfigured},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${MfaExclusion},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.DateTime]
+    # .
+    ${MfaExclusionUntil},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Middlename},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Password},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${PasswordNeverExpires},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISystemuserputPhoneNumbersItem[]]
+    # .
+    # To construct, see NOTES section for PHONENUMBERS properties and create a hash table.
+    ${PhoneNumbers},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${PublicKey},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISystemuserputRelationshipsItem[]]
+    # .
+    ${Relationships},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${SambaServiceUser},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [JumpCloud.SDK.V1.Models.ISshkeypost[]]
+    # .
+    # To construct, see NOTES section for SSHKEYS properties and create a hash table.
+    ${SshKeys},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${Sudo},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Management.Automation.SwitchParameter]
+    # .
+    ${Suspended},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String[]]
+    # .
+    ${Tags},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Int32]
+    # .
+    ${UnixGuid},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.Int32]
+    # .
+    ${UnixUid},
+
+    [Parameter(ParameterSetName='PutExpanded')]
+    [Parameter(ParameterSetName='PutViaIdentityExpanded')]
+    [JumpCloud.SDK.V1.Category('Body')]
+    [System.String]
+    # .
+    ${Username},
+
+    [Parameter()]
+    [JumpCloud.SDK.V1.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Returns true when the command succeeds
+    ${PassThru}
+    )
+    Begin
+    {
+        $Results = @()
+    }
+    Process
+    {
+        $Results = JumpCloud.SDK.V1.internal\Set-JcSdkInternalSystemUser @PSBoundParameters
+    }
+    End
+    {
+        Return $Results
+    }
+}
+
