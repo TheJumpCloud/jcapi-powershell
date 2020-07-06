0a1724a33e04230000f5bff3c314745be22d3408
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-07-06 11:21:40 -0600

    update folder path

--- AutoRest/BuildAutoRest.ps1
+++ AutoRest/BuildAutoRest.ps1
@@ -386,7 +386,14 @@ Try
                 ###########################################################################
                 Set-Location -Path:($OutputFullPath)
                 [System.Version]$CurrentVersion = Get-Metadata -Path:($moduleManifestPath) -PropertyName:('ModuleVersion')
+                If ($PackModule)
+                {
+                    Write-Host ("##vso[task.setvariable variable=ModuleFolder]$extractedModulePath") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
+                }
+                Else
+                {
                 Write-Host ("##vso[task.setvariable variable=ModuleFolder]$OutputFullPath") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
+                }
                 Write-Host ("##vso[task.setvariable variable=VersionMajor]$($CurrentVersion.Major)") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
                 Write-Host ("##vso[task.setvariable variable=VersionMinor]$($CurrentVersion.Minor)") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
                 Write-Host ("##vso[task.setvariable variable=VersionPatch]$($CurrentVersion.Build)") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
