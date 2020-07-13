83f3d6762871e6c70042411124bbc24d298661ad
GitHub <VssAdministrator@FakeEmail.com>
2020-07-13 19:49:51 +0000

    Updating module: JumpCloud.SDK.DirectoryInsights;[skip ci]

--- SDKs/PowerShell/JumpCloud.SDK.DirectoryInsights/custom/Get-JcSdkEventCount.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.DirectoryInsights/custom/Get-JcSdkEventCount.ps1
@@ -4,13 +4,21 @@ Query the API for a count of matching events
 .Description
 Query the API for a count of matching events
 .Example
-PS C:\> {{ Add code here }}
+PS C:\> Get-JcSdkEventCount -Service:('all') -StartTime:((Get-date).AddDays(-30))
 
-{{ Add output here }}
+Pull all event records from a specified time and count the results
 .Example
-PS C:\> {{ Add code here }}
+PS C:\> Get-JcSdkEventCount -Service:('sso') -StartTime:('2020-04-14T00:00:00Z')
 
-{{ Add output here }}
+Pull all SSO event records from a specified time and count the results
+.Example
+PS C:\> Get-JcSdkEventCount -Service:('all') -StartTime:('2020-04-14T00:00:00Z') -EndTime:('2020-04-20T23:00:00Z') -SearchTermAnd @{"event_type" = "admin_login_attempt"; "resource.email" = "admin.user@adminbizorg.com"}
+
+Get all events counts between a date range and match event_type = admin_login_attempt and resource.email = admin.user@adminbizorg.com
+.Example
+PS C:\> Get-JcSdkEventCount -Service:('directory') -StartTime:((Get-date).AddDays(-30)) -searchTermAnd:@{"event_type" = "group_create"}
+
+Get only group_create event counts the last thirty days
 
 .Inputs
 JumpCloud.SDK.DirectoryInsights.Models.IEventQuery
---
+++ SDKs/PowerShell/JumpCloud.SDK.DirectoryInsights/docs/exports/JumpCloud.SDK.DirectoryInsights.md
@@ -0,0 +1,19 @@
+---
+Module Name: JumpCloud.SDK.DirectoryInsights
+Module Guid: 1d8db9dd-ac54-4870-8699-200c21104820
+Download Help Link: https://github.com/TheJumpCloud/jcapi-powershell/tree/master/SDKs/PowerShell/jumpcloud.sdk.directoryinsights
+Help Version: 1.0.0.0
+Locale: en-US
+---
+
+# JumpCloud.SDK.DirectoryInsights Module
+## Description
+The JumpCloud DirectoryInsights PowerShell SDK
+
+## JumpCloud.SDK.DirectoryInsights Cmdlets
+### [Get-JcSdkEvent](Get-JcSdkEvent.md)
+Query the API for Directory Insights events
+
+### [Get-JcSdkEventCount](Get-JcSdkEventCount.md)
+Query the API for a count of matching events
+
