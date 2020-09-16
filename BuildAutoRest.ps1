8cb6aec57a76ff67e64f409029f9981348ec3e71
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-09-16 13:50:21 -0600

    enable tests

--- BuildAutoRest.ps1
+++ BuildAutoRest.ps1
@@ -30,7 +30,7 @@ Try
     $BuildCustomFunctions = $true
     $PrereleaseName = '' # Beta
     $UpdateModuleGuid = $true
-    $TestModule = $false
+    $TestModule = $true
     $ModifyGitIgnore = $true
     $RemoveAzAccounts = $true
     $CommentFormatPs1Xml = $true
