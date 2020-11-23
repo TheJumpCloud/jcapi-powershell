5481cb3675645ed3109a7d9deadb7645c3ea7b70
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-11-23 12:58:27 -0700

    add tests

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/Get-JcSdkApplication.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/Get-JcSdkApplication.Tests.ps1
@@ -1,26 +1,29 @@
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-if (-Not (Test-Path -Path $loadEnvPath)) {
+if (-Not (Test-Path -Path $loadEnvPath))
+{
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
 }
 . ($loadEnvPath)
 $TestRecordingFile = Join-Path $PSScriptRoot 'Get-JcSdkApplication.Recording.json'
 $currentPath = $PSScriptRoot
-while(-not $mockingPath) {
+while (-not $mockingPath)
+{
     $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
     $currentPath = Split-Path -Path $currentPath -Parent
 }
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'Get-JcSdkApplication' {
-    It 'List' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'List' {
+        Get-JcSdkApplication | Should -Not -BeNullOrEmpty
     }
 
-    It 'Get' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Get' {
+        $ListResults = Get-JcSdkApplication
+        Get-JcSdkApplication -Id:($ListResults[0].Id) | Should -Not -BeNullOrEmpty
     }
 
-    It 'GetViaIdentity' -skip {
+    It 'GetViaIdentity' -Skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
 }
