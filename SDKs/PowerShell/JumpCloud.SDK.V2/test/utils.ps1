3ffe4e7a381302496d182742cd94d7d1ea533d32
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2021-02-26 15:03:18 -0700

    Utils and other stuff

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/utils.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/utils.ps1
@@ -37,11 +37,44 @@ function setupEnv() {
     New-JcSdkSystemGroup -Name:($global:SystemGroupName)
     $global:PesterTestSystemGroup = Get-JcSdkSystemGroup | ? { $_.Name -eq $global:SystemGroupName }
 
+    # Select a System
+    $global:PesterTestSystem = Get-JCSdkSystem | Select -First 1
+
+    # Create a RADIUS Server
+    $global:RadiusServerName = "PesterTest"
+    $global:SharedSecret = "Testing123!"
+    $octet1 = 0
+    $octet2 = 0
+    $octet3 = 0
+    $octet4 = 0
+    DO
+    {
+        $global:PesterTestRadiusServer = New-JcSdkRadiusServer -Name:("$global:RadiusServerName") -SharedSecret:($global:SharedSecret) -NetworkSourceIP:("$($octet1).$($octet2).$($octet3).$($octet4)")
+        if ([System.String]::IsNullOrEmpty($global:PesterTestRadiusServer))
+        {
+            if ($octet4 -lt 255) {
+                $octet4++
+            } elseif ($octet3 -lt 244) {
+                $octet4 = 0
+                $octet3++
+            } elseif ($octet2 -lt 244) {
+                $octet3 = 0
+                $octet2++
+            } elseif ($octet1 -lt 244) {
+                $octet2 = 0
+                $octet1++
+            } elseif ($octet1 -eq 255) {
+                Write-Error "Unable to configure RADIUS server - no valid IP Addresses."
+            }
+        }
+    } WHILE ([System.String]::IsNullOrEmpty($global:PesterTestRadiusServer))
+    $Global:RadiusIPAddress = "$($octet1).$($octet2).$($octet3).$($octet4)"
+
 }
 function cleanupEnv() {
     # Clean resources you create for testing
     Remove-JcSdkUser -Id:($global:PesterTestUser.Id)
-
     Remove-JcSdkUserGroup -Id:($global:PesterTestUserGroup.Id)
     Remove-JcSdkSystemGroup -Id:($global:PesterTestSystemGroup.Id)
+    Remove-JcSdkRadiusServer -Id:($global:PesterTestRadiusServer.Id)
 }
