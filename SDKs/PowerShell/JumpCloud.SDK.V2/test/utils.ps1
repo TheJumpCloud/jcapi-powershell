27bf09e8b287df0cce09e8bc6aa33d21968bc291
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2021-02-26 13:13:35 -0700

    Utils

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/utils.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/utils.ps1
@@ -9,16 +9,39 @@ $env = @{}
 function setupEnv() {
     # Preload subscriptionId and tenant from context, which will be used in test
     # as default. You could change them if needed.
-    $env.SubscriptionId = (Get-AzContext).Subscription.Id
-    $env.Tenant = (Get-AzContext).Tenant.Id
+    #### $env.SubscriptionId = (Get-AzContext).Subscription.Id
+    #### $env.Tenant = (Get-AzContext).Tenant.Id
     # For any resources you created for test, you should add it to $env here.
     $envFile = 'env.json'
     if ($TestMode -eq 'live') {
         $envFile = 'localEnv.json'
     }
     set-content -Path (Join-Path $PSScriptRoot $envFile) -Value (ConvertTo-Json $env)
+
+    # Create a user
+    $global:Username = "pester.test"
+    $global:FirstName = "Pester"
+    $global:LastName = "Test"
+    $global:Password = "Testing123!"
+    $global:Email = "pester.test@example.com"
+    New-JcSdkUser -Username:($global:Username) -FirstName:($global:FirstName) -LastName:($global:LastName) -Password:($global:Password) -Email:($global:Email)
+    $global:PesterTestUser = Get-JcSdkUser | ? { $_.username -eq $global:Username }
+
+    # Create a User Group
+    $global:UserGroupName = "PesterTestUserGroup"
+    New-JcSdkUserGroup -Name:($global:UserGroupName)
+    $global:PesterTestUserGroup = Get-JcSdkUserGroup | ? { $_.Name -eq $global:UserGroupName }
+
+    # Create a System Group
+    $global:SystemGroupName = "PesterTestSystemGroup"
+    New-JcSdkSystemGroup -Name:($global:SystemGroupName)
+    $global:PesterTestSystemGroup = Get-JcSdkSystemGroup | ? { $_.Name -eq $global:SystemGroupName }
+
 }
 function cleanupEnv() {
     # Clean resources you create for testing
-}
+    Remove-JcSdkUser -Id:($global:PesterTestUser.Id)
 
+    Remove-JcSdkUserGroup -Id:($global:PesterTestUserGroup.Id)
+    Remove-JcSdkSystemGroup -Id:($global:PesterTestSystemGroup.Id)
+}
