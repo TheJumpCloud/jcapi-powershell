21bac673a5dd83e0ed6a1ba22d5c11fc897330fe
Joe Workman <joe.workman@jumpcloud.com>
2021-03-03 14:09:58 -0700

    set usergroupmember tests

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkUserGroupMember.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkUserGroupMember.Tests.ps1
@@ -12,12 +12,25 @@ while(-not $mockingPath) {
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'Set-JcSdkUserGroupMember' {
-    It 'SetExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'SetExpanded' {
+        Set-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.id) -Id $($global:PesterTestUser.id) -Op 'add'
+        Set-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.id) -Id $($global:PesterTestUser.id) -Op 'remove'
     }
 
-    It 'Set' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Set' {
+        $body = @{
+            Id         = $global:PesterTestUser.Id;
+            Op         = 'add';
+            Type       = 'user';
+            Attributes = @{};
+        }
+        {Set-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.id) -Body $body} | Should -Not -Throw
+        Get-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.Id) | Should -Not -BeNullOrEmpty
+        $body.Op = 'remove'
+        {Set-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.id) -Body $body} | Should -Not -Throw
+        # check that the user was removed from the group
+        $toId = Get-JcSdkUserGroupMember -GroupId $($global:PesterTestUserGroup.Id)
+        $toId.ToId | Should -Not -Contain $($global:PesterTestUser.Id)
     }
 
     It 'SetViaIdentity' -skip {
