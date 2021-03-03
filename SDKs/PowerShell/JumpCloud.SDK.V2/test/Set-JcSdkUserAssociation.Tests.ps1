434582e75e36023530ae030c26d110fd5e5d59bb
Joe Workman <joe.workman@jumpcloud.com>
2021-03-03 15:52:28 -0700

    UserAssociation set Tests

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkUserAssociation.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkUserAssociation.Tests.ps1
@@ -12,12 +12,60 @@ while(-not $mockingPath) {
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'Set-JcSdkUserAssociation' {
-    It 'SetExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    BeforeAll{
+        $types = @{
+            'active_directory' = $($global:PesterTestActiveDirectory.id);
+            'application'      = $($global:PesterTestApplication.id);
+            'g_suite'          = $($global:PesterTestGSuite.id);
+            'ldap_server'      = $($global:PesterTestLdap.id);
+            'office_365'       = $($global:PesterTestOffice365.id);
+            'radius_server'    = $($global:PesterTestRadiusServer.id);
+            'system'           = $($global:PesterTestSystem.id);
+            'system_group'     = $($global:PesterTestSystemGroup.id);
+        }
+    }
+    It 'SetExpanded' {
+        # Adds
+        foreach ($type in $types.keys)
+        {
+            { Set-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Id:$($types[$type]) -Op:('add') -Type:($type) } | Should -Not -Throw
+            # check that the association was added to the group
+            Get-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Targets:($type) | Should -Not -BeNullOrEmpty
+        }
+        # Removes
+        foreach ($type in $types.keys)
+        {
+            { Set-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Id:$($types[$type]) -Op:('remove') -Type:($type) } | Should -Not -Throw
+            # check that the association was removed from the group
+            $toId = Get-JcSdkUserAssociation -UserId $($global:PesterTestUser.Id) -Targets:($type)
+            $toId.ToId | Should -Not -Contain $($types[$type])
+        }
     }
 
-    It 'Set' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Set' {
+        foreach ($type in $types.keys)
+        {
+            $body = @{
+                Id   = $($types[$type])
+                Op   = 'add'
+                Type = $type
+            }
+            { Set-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Body $body } | Should -Not -Throw
+            # check that the association was added to the group
+            Get-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Targets:($type) | Should -Not -BeNullOrEmpty
+        }
+        foreach ($type in $types.keys)
+        {
+            $body = @{
+                Id   = $($types[$type])
+                Op   = 'remove'
+                Type = $type
+            }
+            { Set-JcSdkUserAssociation -UserId:$($global:PesterTestUser.id) -Body $body } | Should -Not -Throw
+            # check that the association was removed from the group
+            $toId = Get-JcSdkUserAssociation -UserId $($global:PesterTestUser.Id) -Targets:($type)
+            $toId.ToId | Should -Not -Contain $($types[$type])
+        }
     }
 
     It 'SetViaIdentity' -skip {
