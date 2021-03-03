ecb44e92c34dbf802e25a43b9123e90db4f6a6d0
Elliott Panipinto <45767377+epanipinto-jc@users.noreply.github.com>
2021-03-03 07:08:29 -0700

    update tests

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/Remove-JcSdkRadiusServer.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/Remove-JcSdkRadiusServer.Tests.ps1
@@ -1,22 +1,24 @@
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-if (-Not (Test-Path -Path $loadEnvPath)) {
+if (-Not (Test-Path -Path $loadEnvPath))
+{
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
 }
 . ($loadEnvPath)
 $TestRecordingFile = Join-Path $PSScriptRoot 'Remove-JcSdkRadiusServer.Recording.json'
 $currentPath = $PSScriptRoot
-while(-not $mockingPath) {
+while (-not $mockingPath)
+{
     $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
     $currentPath = Split-Path -Path $currentPath -Parent
 }
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'Remove-JcSdkRadiusServer' {
-    It 'Delete' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Delete' {
+        { Remove-JcSdkRadiusServer -Id:($global:PesterTestRadiusServer.Id) } | Should -Not -Throw
     }
 
-    It 'DeleteViaIdentity' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'DeleteViaIdentity' -Skip {
+        Remove-JcSdkRadiusServer -InputObject '<IJumpCloudApIsIdentity>'
     }
 }
