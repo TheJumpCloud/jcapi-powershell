08d71677fe2c4caff2fdf085f2c3ca28c1230503
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2021-03-04 12:25:33 -0700

    Couple quick tests

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/Reset-JcSdkUserMfa.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/Reset-JcSdkUserMfa.Tests.ps1
@@ -1,3 +1,5 @@
+# TODO: Configure MFA for $global:PesterTestUser
+
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
 if (-Not (Test-Path -Path $loadEnvPath)) {
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
