b7653751db34d0499d398524982a867600453e49
Elliott Panipinto <45767377+epanipinto-jc@users.noreply.github.com>
2021-03-09 08:09:36 -0700

    update test structure

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/utils.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/utils.ps1
@@ -12,6 +12,10 @@ function RandomString([bool]$allChars, [int32]$len)
 $env = @{}
 function setupEnv()
 {
+    # Preload subscriptionId and tenant from context, which will be used in test
+    # as default. You could change them if needed.
+    $env.SubscriptionId = (Get-AzContext).Subscription.Id
+    $env.Tenant = (Get-AzContext).Tenant.Id
     # For any resources you created for test, you should add it to $env here.
     $envFile = 'env.json'
     if ($TestMode -eq 'live')
@@ -19,70 +23,8 @@ function setupEnv()
         $envFile = 'localEnv.json'
     }
     Set-Content -Path (Join-Path $PSScriptRoot $envFile) -Value (ConvertTo-Json $env)
-    ########################################################################
-    # TODO
-    ########################################################################
-    # (Get-Command -Syntax -Name Invoke-JcSdkCommandTrigger) | ForEach-Object { $_.Replace('[-Fields <string>]', '').Replace('[-Filter <string>]', '').Replace('[-Sort <string>]', '').Replace('[-Search <string>]', '').Replace('[-Paginate <bool>]', '').Replace('[-Break]', '').Replace('[-HttpPipelineAppend <SendAsyncStep[]>]', '').Replace('[-HttpPipelinePrepend <SendAsyncStep[]>]', '').Replace('[-PassThru]', '').Replace('[-Proxy <uri>]', '').Replace('[-ProxyCredential <pscredential>]', '').Replace('[-ProxyUseDefaultCredentials]', '').Replace('[<CommonParameters>]', '').Replace('[-WhatIf]', '').Replace('[-Confirm]', '') }
-    # Get-JcSdkApplication
-    # Get-JcSdkApplicationTemplate
-    # Get-JcSdkCommandFile
-    # Get-JcSdkCommandResult
-    ########################################################################
-    # V1
-    ########################################################################
-    # Create a command
-    $global:PesterDefCommand = @{
-        Name    = 'PesterTestCommand'
-        Command = 'echo "Hello World"'
-        User    = '000000000000000000000000'
-    }
-    # Create a user
-    $global:PesterDefUser = @{
-        Username  = "pester.test.$(RandomString -len 5)"
-        FirstName = "Pester"
-        LastName  = "Test"
-        Password  = "Testing123!"
-        Email     = "pester.test@example$(RandomString -len 5).com"
-    }
-    # Create a user ssh key
-    $global:PesterDefUserSshKey = @{
-        Id        = $null # Defined later in New-JcSdkUserSshKey.Tests.ps1
-        Name      = 'PesterTestUserSshKeyName'
-        PublicKey = 'PesterTestUserSshKeyPublicKey'
-    }
-    # Create a RADIUS Server
-    $global:PesterDefRadiusServer = @{
-        Name            = "PesterTestRadiusServer"
-        SharedSecret    = "Testing123!"
-        NetworkSourceIP = [IPAddress]::Parse([String](Get-Random)).IPAddressToString
-    }
-    # Get organization
-    $global:PesterTestOrganization = Get-JcSdkOrganization
-    # Get a System
-    $global:PesterTestSystem = Get-JcSdkSystem | Select-Object -First 1
 }
 function cleanupEnv()
 {
-    ########################################################################
-    # V1
-    ########################################################################
-    # Remove Commands
-    $null = Get-JcSdkCommand | ForEach-Object { Remove-JcSdkCommand -Id:($_.Id) }
-    # Remove Users and SSH Keys
-    $null = Get-JcSdkUser | ForEach-Object {
-        $UserId = $_.Id
-        If ( $_.ExternallyManaged )
-        {
-            Set-JcSdkUser -Id:($UserId) -ExternallyManaged:($false)
-        }
-        If ($_.SshKeys)
-        {
-            $_.SshKeys | ForEach-Object {
-                Remove-JcSdkUserSshKey -Id:($_.Id) -SystemuserId:($UserId)
-            }
-        }
-        Remove-JcSdkUser -Id:($UserId)
-    }
-    # Remove RADIUS Servers
-    $null = Get-JcSdkRadiusServer | ForEach-Object { Remove-JcSdkRadiusServer -Id:($_.Id) }
+    # Clean resources you create for testing
 }
--- Tools/PesterTests.ps1
+++ Tools/PesterTests.ps1
@@ -1,4 +1,5 @@
 <#
+(Get-Command -Syntax -Name Invoke-JcSdkCommandTrigger) | ForEach-Object { $_.Replace('[-Fields <string>]', '').Replace('[-Filter <string>]', '').Replace('[-Sort <string>]', '').Replace('[-Search <string>]', '').Replace('[-Paginate <bool>]', '').Replace('[-Break]', '').Replace('[-HttpPipelineAppend <SendAsyncStep[]>]', '').Replace('[-HttpPipelinePrepend <SendAsyncStep[]>]', '').Replace('[-PassThru]', '').Replace('[-Proxy <uri>]', '').Replace('[-ProxyCredential <pscredential>]', '').Replace('[-ProxyUseDefaultCredentials]', '').Replace('[<CommonParameters>]', '').Replace('[-WhatIf]', '').Replace('[-Confirm]', '') }
 TODO:
     Clear-JcSdkSystem.Tests.ps1: Setup Orgs with a device that can be cleared each time
     Get-JcSdkCommandFile.Tests.ps1: Configure New-JcSdkCommand test to upload a simple text file
@@ -9,7 +10,49 @@ TODO:
     Restart-JcSdkSystem.Tests.ps1: Setup Orgs with a device that can be restarted each time
     Remove-JcSdkSystem.Tests.ps1: Disabled untill we can create systems as well
 #>
-#################################################################
+$testFolder = $testFolder # .\jcapi-powershell\SDKs\PowerShell\JumpCloud.SDK.V1\test
+$moduleName = $moduleName # JumpCloud.SDK.V1
+#region Define Objects
+If ($moduleName -eq 'JumpCloud.SDK.V1')
+{
+    # Create a command
+    $global:PesterDefCommand = @{
+        Name    = 'PesterTestCommand'
+        Command = 'echo "Hello World"'
+        User    = '000000000000000000000000'
+    }
+    # Create a user
+    $global:PesterDefUser = @{
+        Username  = "pester.test.$(RandomString -len 5)"
+        FirstName = "Pester"
+        LastName  = "Test"
+        Password  = "Testing123!"
+        Email     = "pester.test@example$(RandomString -len 5).com"
+    }
+    # Create a user ssh key
+    $global:PesterDefUserSshKey = @{
+        Id        = $null # Defined later in New-JcSdkUserSshKey.Tests.ps1
+        Name      = 'PesterTestUserSshKeyName'
+        PublicKey = 'PesterTestUserSshKeyPublicKey'
+    }
+    # Create a RADIUS Server
+    $global:PesterDefRadiusServer = @{
+        Name            = "PesterTestRadiusServer"
+        SharedSecret    = "Testing123!"
+        NetworkSourceIP = [IPAddress]::Parse([String](Get-Random)).IPAddressToString
+    }
+    # Get organization
+    $global:PesterTestOrganization = Get-JcSdkOrganization
+    # Get a System
+    $global:PesterTestSystem = Get-JcSdkSystem | Select-Object -First 1
+}
+ElseIf ($moduleName -eq 'JumpCloud.SDK.V2')
+{
+
+}
+#endregion Define Objects
+
+#region Run Pester Tests
 # If V2 is being run then run the V1 tests also because of dependencies
 If ($moduleName -eq 'JumpCloud.SDK.V2')
 {
@@ -33,4 +76,29 @@ $OrderedTestsTakeDown | ForEach-Object { $FileBaseName = $_; $PesterTestFiles +=
 $PesterTestFiles += $TestFiles | Where-Object { $_.BaseName -like "Remove-*" -and $_.BaseName -notin $OrderedTestsTakeDown }
 # Run tests
 Invoke-Pester -Script $PesterTestFiles.FullName -EnableExit -OutputFile (Join-Path $testFolder "$moduleName-TestResults.xml")
-#################################################################
+#endregion Run Pester Tests
+
+# #region Clean Up
+# If ($moduleName -eq 'JumpCloud.SDK.V1')
+# {
+#     # Remove Commands
+#     $null = Get-JcSdkCommand | ForEach-Object { Remove-JcSdkCommand -Id:($_.Id) }
+#     # Remove Users and SSH Keys
+#     $null = Get-JcSdkUser | ForEach-Object {
+#         $UserId = $_.Id
+#         If ( $_.ExternallyManaged )
+#         {
+#             Set-JcSdkUser -Id:($UserId) -ExternallyManaged:($false)
+#         }
+#         If ($_.SshKeys)
+#         {
+#             $_.SshKeys | ForEach-Object {
+#                 Remove-JcSdkUserSshKey -Id:($_.Id) -SystemuserId:($UserId)
+#             }
+#         }
+#         Remove-JcSdkUser -Id:($UserId)
+#     }
+#     # Remove RADIUS Servers
+#     $null = Get-JcSdkRadiusServer | ForEach-Object { Remove-JcSdkRadiusServer -Id:($_.Id) }
+# }
+# #endregion Clean Up
