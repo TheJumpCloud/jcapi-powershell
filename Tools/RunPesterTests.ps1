0ecb1071fb2d28acf777ec9f80d74e283440c6b6
Elliott Panipinto <45767377+epanipinto-jc@users.noreply.github.com>
2021-03-17 09:55:05 -0600

    remove testing notes

--- Tools/RunPesterTests.ps1
+++ Tools/RunPesterTests.ps1
@@ -37,9 +37,7 @@ $moduleName = $moduleName # JumpCloud.SDK.V1
 #region Import Modules
 If ($moduleName -eq 'JumpCloud.SDK.V2')
 {
-    Write-Host ("Hi1: $($modulePath)")
-    Write-Host ("Hi2: $($modulePath.Replace('JumpCloud.SDK.V2', 'JumpCloud.SDK.V1'))")
-    Import-Module -Name $modulePath.Replace('JumpCloud.SDK.V2', 'JumpCloud.SDK.V1') -Force -Verbose
+    Import-Module -Name $modulePath.Replace('JumpCloud.SDK.V2', 'JumpCloud.SDK.V1') -Force
 }
 #endregion Import Modules
 #region Define Objects
