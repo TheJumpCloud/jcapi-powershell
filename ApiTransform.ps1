76920ab85c7ec08c163a7dc9bc2f30b6b67fcc87
kmoorehead-jc <43962331+kmoorehead-jc@users.noreply.github.com>
2021-03-31 13:21:12 -0600

    Back to singular remove

--- ApiTransform.ps1
+++ ApiTransform.ps1
@@ -364,7 +364,7 @@ $TransformConfig = [Ordered]@{
             'GET_workdays-id-import-job_id-results'                      = 'Import-WorkdayResult';
             'GET_workdays-workday_id-workers'                            = 'Get-WorkdayWorker';
         };
-        ExcludedList       = @('/applications/{application_id}', '/applications/{application_id}/logo', '/logos/{id}', 'applemdms/{apple_mdm_id}/enrollmentprofiles/{id}')
+        ExcludedList       = @('/applications/{application_id}', '/applications/{application_id}/logo', '/logos/{id}', '/applemdms/{apple_mdm_id}/enrollmentprofiles/{id}')
     }
 }
 Function Get-SwaggerItem
