24d5280b93b43a076c9749e234b71e481b69a16e
Elliott Panipinto <45767377+epanipinto-jc@users.noreply.github.com>
2021-05-19 08:07:59 -0600

    fix tests

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/Get-JcSdkSystemGroupTraversePolicyGroup.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/Get-JcSdkSystemGroupTraversePolicyGroup.Tests.ps1
@@ -1,24 +1,22 @@
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-if (-Not (Test-Path -Path $loadEnvPath))
-{
+if (-Not (Test-Path -Path $loadEnvPath)) {
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
 }
 . ($loadEnvPath)
-$TestRecordingFile = Join-Path $PSScriptRoot 'New-JcSdkActiveDirectory.Recording.json'
+$TestRecordingFile = Join-Path $PSScriptRoot 'Get-JcSdkSystemGroupTraversePolicyGroup.Recording.json'
 $currentPath = $PSScriptRoot
-while (-not $mockingPath)
-{
+while(-not $mockingPath) {
     $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
     $currentPath = Split-Path -Path $currentPath -Parent
 }
 . ($mockingPath | Select-Object -First 1).FullName
 
-Describe 'New-JcSdkActiveDirectory' {
-    It 'NewExpanded' -Skip {
+Describe 'Get-JcSdkSystemGroupTraversePolicyGroup' {
+    It 'Get' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
 
-    It 'New' -Skip {
+    It 'GetViaIdentity' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
 }
--- SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkActiveDirectory.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkActiveDirectory.Tests.ps1
@@ -1,10 +1,14 @@
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-if (-Not (Test-Path -Path $loadEnvPath))
-{
+if (-Not (Test-Path -Path $loadEnvPath)) {
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
 }
 . ($loadEnvPath)
 $TestRecordingFile = Join-Path $PSScriptRoot 'New-JcSdkActiveDirectory.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'New-JcSdkActiveDirectory' {
