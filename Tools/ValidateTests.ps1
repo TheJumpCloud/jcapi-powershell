93b8e8ceb29136e00e23d1881c17d4929eb6ab6d
Joe Workman <joe.workman@jumpcloud.com>
2022-03-11 10:43:51 -0500

    First Pass with MTP tests seperated by Tags

--- Tools/ValidateTests.ps1
+++ Tools/ValidateTests.ps1
@@ -8,6 +8,7 @@ Begin
     Write-Host $Path
     # Regex search for tests
     $regex = [regex]"It.*{"
+    $pesterRegex = [regex]"([^;]*)Describe"
     # Test List
     $TestResults = @()
 }
@@ -26,10 +27,17 @@ Process{
                 $notSkipped += 1
             }
         }
+        # Search for code outside describe block
+        $raw = Get-Content -Path ($test.FullName)
+        $rogueText = [regex]::match($raw, $pesterRegex)
+        if ($rogueText.Value -notmatch "BeforeAll"){
+            Write-Warning "Pester test: $($test.BaseName) contains code outside valid blocks; This test may not execute"
+        }
         $TestResults += [PSCustomObject]@{
             TestName = $($test.BaseName)
             Tests    = "($notSkipped / $count)"
             Coverage = if (($notSkipped / $count -eq 1)) { "$([char]0x1b)[92mOK" }elseif ($count - $notSkipped -lt $count) { "$([char]0x1b)[93m-" }else { "$([char]0x1b)[91mx" }
+            "Pester 5 Compatible"   = if ($rogueText.Value -notmatch "BeforeAll") { "$([char]0x1b)[91mx" }else { "$([char]0x1b)[92mOK" }
         }
     }
 }
