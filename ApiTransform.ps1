e823eb53343ad0ea061be5957a3764be710363da
Joe Workman <joe.workman@jumpcloud.com>
2022-03-24 13:59:41 -0600

    ApiTransform updates, account for incorrect response codes

--- ApiTransform.ps1
+++ ApiTransform.ps1
@@ -113,7 +113,8 @@ $TransformConfig = [Ordered]@{
             '"in":"body","name":"body","schema":{"\$ref":"#\/definitions\/CustomEmail"}'          = '"in":"body","name":"CustomEmail","schema":{"$ref":"#/definitions/CustomEmail"}'; # The type 'SetJcSdkInternalCustomEmailConfiguration_SetExpanded, SetJcSdkInternalCustomEmailConfiguration_SetViaIdentityExpanded, NewJcSdkInternalCustomEmailConfiguration_CreateExpanded' already contains a definition for 'Body'
             '"format":"uint32"'                                                                   = '"format":"int64"' # SI code uses uint32 which is larger than int32 . Swagger 2 doesnt have a concept of uint32 . AutoRest defaults to int32 when it sees a type of integer.
             # Custom Tweaks
-            '"responses":{"201":{"description":"","schema":{"\$ref":"#\/definitions\/job-id"}}'   = '"responses":{"200":{"description":"OK","schema":{"$ref":"#/definitions/job-id"}}'; # Workaround incorrectly defined 201 response in swagger should be 200
+            '"responses":{"201":{"description":"","schema":{"\$ref":"#\/definitions\/job-id"}}'   = '"responses":{"200":{"description":"OK","schema":{"$ref":"#/definitions/job-id"}}'; # Workaround incorrectly defined 201 response in swagger should be 200; affects New-JcSdkBulkUser
+            '"responses":{"201":{"description":"OK","schema":{"items":{"\$ref":"#\/definitions\/AdministratorOrganizationLink"}' = '"responses":{"200":{"description":"OK","schema":{"items":{"$ref":"#/definitions/AdministratorOrganizationLink"}' # Workaround incorrectly defined 201 resposne in swagger, should be 200; affects Get-JcSdkAdministratorOrganization
             '{"\$ref":"#\/parameters\/trait:requestHeaders:Content-Type"}'                        = ''; # This will be passed in later through the Module.cs file.
             '{"\$ref":"#\/parameters\/trait:requestHeaders:Accept"}'                              = ''; # This will be passed in later through the Module.cs file.
             '{"\$ref":"#\/parameters\/trait:multiTenantRequestHeaders:x-org-id"}'                 = ''; # Along with the ApiKey this will be passed in later through the Module.cs file.
@@ -446,6 +447,11 @@ $TransformConfig = [Ordered]@{
             '/integrations/autotask/{UUID}',
             '/integrations/autotask/{UUID}/settings',
             '/integrations/autotask/{UUID}/companytypes',
+            '/integrations/autotask/{UUID}/companies',
+            '/integrations/autotask/{UUID}/contracts',
+            '/integrations/autotask/{UUID}/contracts/fields',
+            '/integrations/autotask/{UUID}/contracts/services',
+            '/integrations/autotask/{UUID}/mappings',
             '/providers/{provider_id}/integrations/autotask')
     }
 }
