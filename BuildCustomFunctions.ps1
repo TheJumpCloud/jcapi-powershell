0043b669bd425ef2a6fd7d20dcae59a5451aab24
Joe Workman <joe.workman@jumpcloud.com>
2022-12-15 10:23:24 -0700

    build custom functions : temp fix for DE-2630/ tests

--- BuildCustomFunctions.ps1
+++ BuildCustomFunctions.ps1
@@ -359,25 +359,33 @@ $($IndentChar)$($IndentChar)$($IndentChar)$($IndentChar)Return `$ResponseTask
 $($IndentChar)$($IndentChar)$($IndentChar)}
 $($IndentChar)$($IndentChar))"
                 # Build "Process" block
+                # temp fix for Set-JcSdkUserAssociation TODO: remove when DE-2630 is done
+                if ($NewCommandName -eq "Set-JcSdkUserAssociation") {
+                    $ProcessContent += "$($IndentChar)$($IndentChar)if ((-not `$PSBoundParameters['attributes']) -and (`$PSBoundParameters['op'] -eq 'remove')) {
+                    $($IndentChar)$($IndentChar)$($IndentChar)`$PSBoundParameters.Add('attributes', @{ '' = '' })
+                    $($IndentChar)$($IndentChar)}"
+                }
                 $ProcessContent += "$($IndentChar)$($IndentChar)`$Results = $ResultsLogic"
                 # Build "End" block
                 $EndContent += "$($IndentChar)$($IndentChar)Write-Debug ('HttpRequest: ' + `$JCHttpRequest);
-$($IndentChar)$($IndentChar)# Write-Debug ('HttpRequestContent: ' + `$JCHttpRequestContent.Result);
-$($IndentChar)$($IndentChar)Write-Debug ('HttpResponse: ' + `$JCHttpResponse.Result);
-$($IndentChar)$($IndentChar)# Write-Debug ('HttpResponseContent: ' + `$JCHttpResponseContent.Result);
-$($IndentChar)$($IndentChar)# Clean up global variables
-$($IndentChar)$($IndentChar)`$GlobalVars = @('JCHttpRequest', 'JCHttpRequestContent', 'JCHttpResponse', 'JCHttpResponseContent')
-$($IndentChar)$($IndentChar)`$GlobalVars | ForEach-Object {
-$($IndentChar)$($IndentChar)$($IndentChar)If ((Get-Variable -Scope:('Global')).Where( { `$_.Name -eq `$_ })) { Remove-Variable -Name:(`$_) -Scope:('Global') }
-$($IndentChar)$($IndentChar)}
-$($IndentChar)$($IndentChar)Return `$Results"
+                $($IndentChar)$($IndentChar)# Write-Debug ('HttpRequestContent: ' + `$JCHttpRequestContent.Result);
+                $($IndentChar)$($IndentChar)Write-Debug ('HttpResponse: ' + `$JCHttpResponse.Result);
+                $($IndentChar)$($IndentChar)# Write-Debug ('HttpResponseContent: ' + `$JCHttpResponseContent.Result);
+                $($IndentChar)$($IndentChar)# Clean up global variables
+                $($IndentChar)$($IndentChar)`$GlobalVars = @('JCHttpRequest', 'JCHttpRequestContent', 'JCHttpResponse', 'JCHttpResponseContent')
+                $($IndentChar)$($IndentChar)`$GlobalVars | ForEach-Object {
+                    $($IndentChar)$($IndentChar)$($IndentChar)If ((Get-Variable -Scope:('Global')).Where( { `$_.Name -eq `$_ })) {
+                        Remove-Variable -Name:(`$_) -Scope:('Global')
+                    }
+                    $($IndentChar)$($IndentChar) }
+                $($IndentChar)$($IndentChar)Return `$Results"
             } Else {
                 Write-Error ("Unmapped command: $CommandName")
                 $null
             }
             If (-not [System.String]::IsNullOrEmpty($BeginContent) -and -not [System.String]::IsNullOrEmpty($ProcessContent) -and -not [System.String]::IsNullOrEmpty($EndContent)) {
                 # Build "Function"
-                $NewScript = $FunctionTemplate -f '', $NewCommandName, $CmdletBinding, ($ParameterContent -join ",`n`n"), ($BeginContent -join "`n"), ($ProcessContent -join "`n"), ($EndContent -join "`n")
+                $NewScript = $FunctionTemplate -f '', $NewCommandName, $CmdletBinding, ($ParameterContent -join ", `n`n"), ($BeginContent -join "`n"), ($ProcessContent -join "`n"), ($EndContent -join "`n")
                 # Fix line endings
                 $NewScript = $NewScript.Replace("`r`n", "`n").Trim()
                 # Export the function
