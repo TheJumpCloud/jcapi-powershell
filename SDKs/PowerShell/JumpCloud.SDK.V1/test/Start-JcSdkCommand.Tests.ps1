16567b3860ed812dae068c43dd854443ee7585ce
Ken Maranion <ken.maranion@jumpcloud.com>
2023-11-10 14:12:06 -0800

    Add run command test

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/Start-JcSdkCommand.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/Start-JcSdkCommand.Tests.ps1
@@ -1,25 +1,23 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Start-JcSdkCommand'))
-{
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Start-JcSdkCommand.Recording.json'
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+BeforeAll {
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) {
+        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+    }
+    . ($loadEnvPath)
+    $TestRecordingFile = Join-Path $PSScriptRoot 'Set-JcSdkCommand.Recording.json'
+    $currentPath = $PSScriptRoot
+    while(-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'Start-JcSdkCommand' {
-    It 'RunExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+Describe 'Set-JcSdkCommand' -Tag:(""){
+    It 'Start a command just by command Id' {
+        Start-JcSdkCommand -Id:($global:PesterTestCommand.Id) | Should -Not -Throw
     }
-
-    It 'Run' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Start a command by command Id and system Id' {
+        Start-JcSdkCommand -Id:($global:PesterTestCommand.Id) -SystemId:($global:PesterTestSystem.Id) | Should -Not -Throw
     }
 }
