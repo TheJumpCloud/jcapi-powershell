e59d2587313fbfaea1ea76a19a8390ea939868c9
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2024-04-01 11:58:18 -0600

    update tests

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkBulkUser.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkBulkUser.Tests.ps1
@@ -18,7 +18,36 @@ Describe 'New-JcSdkBulkUser' -Tag:(""){
     It 'Create' {
         $global:PesterTestBulkUserJobId =  New-JcSdkBulkUser -Body:($global:PesterDefBulkUser) -CreationSource:('jumpcloud:bulk')
         { $global:PesterTestBulkUserJobId } | Should -Not -BeNullOrEmpty
-        { Get-JcSdkUser -id $global:PesterTestBulkUserJobId.Id } | Should -Not -BeNullOrEmpty
+        $bulkUserCreate = Get-JcSdkUser -Filter "username:`$eq:$($global:PesterDefBulkUser.username)"
+        $bulkUserCreate.AccountLocked | Should -Be $global:PesterDefBulkUser.AccountLocked
+        $bulkUserCreate.Activated | Should -Be $global:PesterDefBulkUser.Activated
+        $bulkUserCreate.Addresses | Should -Be $global:PesterDefBulkUser.Addresses
+        $bulkUserCreate.AllowPublicKey | Should -Be $global:PesterDefBulkUser.AllowPublicKey
+        $bulkUserCreate.AlternateEmail | Should -Be $global:PesterDefBulkUser.AlternateEmail
+        $bulkUserCreate.Company | Should -Be $global:PesterDefBulkUser.Company
+        $bulkUserCreate.CostCenter | Should -Be $global:PesterDefBulkUser.CostCenter
+        $bulkUserCreate.Department | Should -Be $global:PesterDefBulkUser.Department
+        $bulkUserCreate.Description | Should -Be $global:PesterDefBulkUser.Description
+        $bulkUserCreate.DisableDeviceMaxLoginAttempts | Should -Be $global:PesterDefBulkUser.DisableDeviceMaxLoginAttempts
+        $bulkUserCreate.Displayname | Should -Be $global:PesterDefBulkUser.DisplayName
+        $bulkUserCreate.Email | Should -Be $global:PesterDefBulkUser.Email
+        $bulkUserCreate.EmployeeIdentifier | Should -Be $global:PesterDefBulkUser.EmployeeIdentifier
+        $bulkUserCreate.EmployeeType | Should -Be $global:PesterDefBulkUser.EmployeeType
+        $bulkUserCreate.EnableManagedUid | Should -Be $global:PesterDefBulkUser.EnableManagedUid
+        $bulkUserCreate.EnableUserPortalMultifactor | Should -Be $global:PesterDefBulkUser.EnableUserPortalMultifactor
+        $bulkUserCreate.ExternallyManaged | Should -Be $global:PesterDefBulkUser.ExternallyManaged
+        $bulkUserCreate.Firstname | Should -Be $global:PesterDefBulkUser.FirstName
+        $bulkUserCreate.JobTitle | Should -Be $global:PesterDefBulkUser.JobTitle
+        $bulkUserCreate.Lastname | Should -Be $global:PesterDefBulkUser.LastName
+        $bulkUserCreate.LdapBindingUser | Should -Be $global:PesterDefBulkUser.LdapBindingUser
+        $bulkUserCreate.Location | Should -Be $global:PesterDefBulkUser.Location
+        $bulkUserCreate.ManagedAppleId | Should -Be $global:PesterDefBulkUser.ManagedAppleId
+        $bulkUserCreate.Manager | Should -Be $global:PesterDefBulkUser.Manager
+        $bulkUserCreate.MfaConfigured | Should -Be $global:PesterDefBulkUser.MfaConfigured
+        $bulkUserCreate.PasswordNeverExpires | Should -Be $global:PesterDefBulkUser.PasswordNeverExpires
+        $bulkUserCreate.PasswordlessSudo | Should -Be $global:PesterDefBulkUser.PasswordlessSudo
+        $bulkUserCreate.PhoneNumbers | Should -Be $global:PesterDefBulkUser.PhoneNumbers
+        $bulkUserCreate.Username | Should -Be $global:PesterDefBulkUser.Username
     }
 }
 
--- SDKs/PowerShell/JumpCloud.SDK.V2/test/Update-JcSdkBulkUser.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/Update-JcSdkBulkUser.Tests.ps1
@@ -15,7 +15,38 @@ while(-not $mockingPath) {
 
 }
 Describe 'Update-JcSdkBulkUser' -Tag:(""){
-    It 'Update' -skip {
-        { Update-JcSdkBulkUser -Body:($global:PesterDefUpdateBulkUser) } | Should -Not -Throw
+    It 'Update' {
+        $updateBulkUserJobID = Update-JcSdkBulkUser -Body:($global:PesterDefUpdateBulkUser) | Should -Not -Throw
+        $updateBulkUserJobID | Should -Not -BeNullOrEmpty
+        $bulkUserUpdate = Get-JcSdkUser -Filter "username:`$eq:$($global:PesterDefUpdateBulkUser.username)"
+        $bulkUserUpdate.AccountLocked | Should -Be $global:PesterDefUpdateBulkUser.AccountLocked
+        $bulkUserUpdate.Activated | Should -Be $global:PesterDefUpdateBulkUser.Activated
+        $bulkUserUpdate.Addresses | Should -Be $global:PesterDefUpdateBulkUser.Addresses
+        $bulkUserUpdate.AllowPublicKey | Should -Be $global:PesterDefUpdateBulkUser.AllowPublicKey
+        $bulkUserUpdate.AlternateEmail | Should -Be $global:PesterDefUpdateBulkUser.AlternateEmail
+        $bulkUserUpdate.Company | Should -Be $global:PesterDefUpdateBulkUser.Company
+        $bulkUserUpdate.CostCenter | Should -Be $global:PesterDefUpdateBulkUser.CostCenter
+        $bulkUserUpdate.Department | Should -Be $global:PesterDefUpdateBulkUser.Department
+        $bulkUserUpdate.Description | Should -Be $global:PesterDefUpdateBulkUser.Description
+        $bulkUserUpdate.DisableDeviceMaxLoginAttempts | Should -Be $global:PesterDefUpdateBulkUser.DisableDeviceMaxLoginAttempts
+        $bulkUserUpdate.Displayname | Should -Be $global:PesterDefUpdateBulkUser.DisplayName
+        $bulkUserUpdate.Email | Should -Be $global:PesterDefUpdateBulkUser.Email
+        $bulkUserUpdate.EmployeeIdentifier | Should -Be $global:PesterDefUpdateBulkUser.EmployeeIdentifier
+        $bulkUserUpdate.EmployeeType | Should -Be $global:PesterDefUpdateBulkUser.EmployeeType
+        $bulkUserUpdate.EnableManagedUid | Should -Be $global:PesterDefUpdateBulkUser.EnableManagedUid
+        $bulkUserUpdate.EnableUserPortalMultifactor | Should -Be $global:PesterDefUpdateBulkUser.EnableUserPortalMultifactor
+        $bulkUserUpdate.ExternallyManaged | Should -Be $global:PesterDefUpdateBulkUser.ExternallyManaged
+        $bulkUserUpdate.Firstname | Should -Be $global:PesterDefUpdateBulkUser.FirstName
+        $bulkUserUpdate.JobTitle | Should -Be $global:PesterDefUpdateBulkUser.JobTitle
+        $bulkUserUpdate.Lastname | Should -Be $global:PesterDefUpdateBulkUser.LastName
+        $bulkUserUpdate.LdapBindingUser | Should -Be $global:PesterDefUpdateBulkUser.LdapBindingUser
+        $bulkUserUpdate.Location | Should -Be $global:PesterDefUpdateBulkUser.Location
+        $bulkUserUpdate.ManagedAppleId | Should -Be $global:PesterDefUpdateBulkUser.ManagedAppleId
+        $bulkUserUpdate.Manager | Should -Be $global:PesterDefUpdateBulkUser.Manager
+        $bulkUserUpdate.MfaConfigured | Should -Be $global:PesterDefUpdateBulkUser.MfaConfigured
+        $bulkUserUpdate.PasswordNeverExpires | Should -Be $global:PesterDefUpdateBulkUser.PasswordNeverExpires
+        $bulkUserUpdate.PasswordlessSudo | Should -Be $global:PesterDefUpdateBulkUser.PasswordlessSudo
+        $bulkUserUpdate.PhoneNumbers | Should -Be $global:PesterDefUpdateBulkUser.PhoneNumbers
+        $bulkUserUpdate.Username | Should -Be $global:PesterDefUpdateBulkUser.Username
     }
 }
