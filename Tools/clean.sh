979fae570dbad9d568fbfde40f13058ff6b4b1c7
Joe Workman <joe.workman@jumpcloud.com>
2025-11-10 15:49:10 -0700

    autospell

--- Tools/clean.sh
+++ Tools/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Autorest will ocassionally generate items in /obj /bin that the current user
+# Autorest will occasionally generate items in /obj /bin that the current user
 # in MacOS does not have permissions to remove. This script will set permissions
 # on /sdk.*/bin and /sdk.*/obj and remove these directories so the
 # build-module.ps1 script can run without throwing an error.
