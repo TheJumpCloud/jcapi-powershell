d56ae7acf5e858e6d8242a390482d4a3b8d2316f
Joe Workman <joe.workman@jumpcloud.com>
2025-11-24 11:55:42 -0700

    use display name for filters since these are unique

--- SDKs/PowerShell/JumpCloud.SDK.V1/test/Get-JcSdkApplication.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V1/test/Get-JcSdkApplication.Tests.ps1
@@ -21,14 +21,14 @@ Describe 'Get-JcSdkApplication' -Tag:(""){
     It 'List Filter Tests' {
         $app = Get-JcSdkApplication | Select-Object -First 1
         # Test single conditions
-        $NameEQ = Get-JcsdkApplication -Filter @("name:`$eq:$($app.Name)")
+        $NameEQ = Get-JcsdkApplication -Filter @("displayName:`$eq:$($app.DisplayName)")
         $NameEQ | Should -Not -BeNullOrEmpty
-        $NameNE = Get-JcsdkApplication -Filter @("name:`$ne:$($app.Name)")
+        $NameNE = Get-JcsdkApplication -Filter @("displayName:`$ne:$($app.DisplayName)")
         $nameNE  | Should -Not -BeNullOrEmpty
         $nameNE | Should -Not -Contain $NameEQ
         # Multiple Filter Conditions
-        Get-JcsdkApplication -Filter @("name:`$eq:$($app.Name)", "_id:`$eq:$($app.Id)") | Should -Not -BeNullOrEmpty
-        Get-JcsdkApplication -Filter @("name:`$eq:$($app.Name)", "_id:`$eq:$($app.Organization)") | Should -BeNullOrEmpty
+        Get-JcsdkApplication -Filter @("displayName:`$eq:$($app.DisplayName)", "_id:`$eq:$($app.Id)") | Should -Not -BeNullOrEmpty
+        Get-JcsdkApplication -Filter @("displayName:`$eq:$($app.DisplayName)", "_id:`$eq:$($app.Organization)") | Should -BeNullOrEmpty
     }
 
     It 'Get' {
