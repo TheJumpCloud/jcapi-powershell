9f4a1d8a00aba653dadb668de39c9442d1a3f365
shashisinghjc <shashi.singh@jumpcloud.com>
2026-02-23 11:58:33 +0530

    Tests updated for New-JcSdkApprovalFlow

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkApprovalFlow.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/New-JcSdkApprovalFlow.Tests.ps1
@@ -14,12 +14,61 @@ if(($null -eq $TestName) -or ($TestName -contains 'New-JcSdkApprovalFlow'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'New-JcSdkApprovalFlow' {
-    It 'CreateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+function Get-TestApprovalFlowGroupContext {
+    $userGroups = @(Get-JcSdkUserGroup | Where-Object { $_ })
+
+    while ($userGroups.Count -lt 2) {
+        $suffix = [guid]::NewGuid().ToString('N').Substring(0,8)
+        $userGroups += New-JcSdkUserGroup -Name "PesterApprovalFlowGroup-$suffix"
     }
 
-    It 'Create' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    $resourceGroup = $userGroups | Get-Random
+    $visibleGroup = Get-JcSdkUserGroup -Filter @('name:eq:All Activated Users') | Select-Object -First 1
+
+    if (-not $visibleGroup -or $visibleGroup.Id -eq $resourceGroup.Id) {
+        $visibleCandidates = $userGroups | Where-Object { $_.Id -ne $resourceGroup.Id }
+        if ($visibleCandidates) {
+            $visibleGroup = $visibleCandidates | Get-Random
+        }
+        else {
+            $visibleGroup = $resourceGroup
+        }
     }
+
+    [pscustomobject]@{
+        ResourceGroup = $resourceGroup
+        VisibleGroup  = $visibleGroup
+    }
+}
+Describe 'New-JcSdkApprovalFlow' -Tag:(""){
+    It 'Create new approval flow with required parameters' {
+        $context = Get-TestApprovalFlowGroupContext
+        $flowName = "PesterApprovalFlow-$([guid]::NewGuid().ToString('N').Substring(0,8))"
+        $description = "Created new approval flow with required parameters on $(Get-Date -Format 'u')"
+
+        $parameters = @{
+            ApprovalType        = 'manual'
+            Category            = 'Application'
+            Description         = $description
+            MultiSelectDuration = @('P5D')
+            Name                = $flowName
+            NonAdminApproval    = $false
+            ResourceId          = $context.ResourceGroup.Id
+            ResourceType        = 'user_group'
+            SlackEnabled        = $false
+            Status              = 'active'
+            TimeBasedAccess     = $true
+            TtlConfig           = 'TTL_CONFIG_MULTI_SELECT_DURATIONS'
+            VisibleTo           = @($context.VisibleGroup.Id)
+        }
+
+        $approvalFlow = New-JcSdkApprovalFlow @parameters
+
+        $approvalFlow | Should -Not -BeNullOrEmpty
+        $approvalFlow.Name | Should -Be $flowName
+        $approvalFlow.ResourceId | Should -Be $context.ResourceGroup.Id
+        $approvalFlow.VisibleTo | Should -Contain $context.VisibleGroup.Id
+    }
+
+
 }
