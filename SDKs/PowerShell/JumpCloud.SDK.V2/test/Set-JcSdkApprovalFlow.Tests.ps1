8d99e39798360eab09e5845f7b3ee23e34c39793
shashisinghjc <shashi.singh@jumpcloud.com>
2026-02-23 11:30:39 +0530

    Tests updated for Set-JcSdkApprovalflow

--- SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkApprovalFlow.Tests.ps1
+++ SDKs/PowerShell/JumpCloud.SDK.V2/test/Set-JcSdkApprovalFlow.Tests.ps1
@@ -7,6 +7,7 @@ if(($null -eq $TestName) -or ($TestName -contains 'Set-JcSdkApprovalFlow'))
   . ($loadEnvPath)
   $TestRecordingFile = Join-Path $PSScriptRoot 'Set-JcSdkApprovalFlow.Recording.json'
   $currentPath = $PSScriptRoot
+  $mockingPath = $null
   while(-not $mockingPath) {
       $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
       $currentPath = Split-Path -Path $currentPath -Parent
@@ -14,20 +15,69 @@ if(($null -eq $TestName) -or ($TestName -contains 'Set-JcSdkApprovalFlow'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'Set-JcSdkApprovalFlow' {
-    It 'SetExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
 
-    It 'Set' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
+Describe 'Set-JcSdkApprovalFlow' -Tag:(""){
+    It 'Set' {
+        $workflows = Get-JcSdkApprovalFlow
+        if (-not $workflows) {
+            $userGroups = Get-JcSdkUserGroup
+            $resourceGroup = $null
+            $visibleGroup = $null
+            if (-not $userGroups) {
+                $createdGroups = @()
+                1..2 | ForEach-Object {
+                    $groupName = "PesterFlowGroup$_-$([guid]::NewGuid().ToString('N').Substring(0,8))"
+                    $createdGroups += New-JcSdkUserGroup -Name $groupName
+                }
 
-    It 'SetViaIdentity' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
+                $userGroups = $createdGroups
+                $resourceGroup = $createdGroups[0]
+                $visibleGroup = $createdGroups[1]
+            }
 
-    It 'SetViaIdentityExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+            $workflowNameSuffix = [guid]::NewGuid().ToString('N').Substring(0,8)
+            $workflows = @(
+                New-JcSdkApprovalFlow -ApprovalType 'manual' -Category 'Application' -Description 'A new workflow2' -MultiSelectDuration @('P5D') -Name "New Workflow-$workflowNameSuffix" -ResourceId $resourceGroup.Id -ResourceType 'user_group' -Status 'active' -TimeBasedAccess -TtlConfig 'TTL_CONFIG_MULTI_SELECT_DURATIONS' -VisibleTo @($visibleGroup.Id)
+            )
+        }
+
+        $workflow = $workflows | Get-Random
+        $newName = "{0}-Updated" -f $workflow.Name
+        $newDescription = "Updated via Pester on $(Get-Date -Format 'u')"
+
+        $body = @{
+            ApprovalType        = $workflow.ApprovalType
+            ApproverRequirement = $workflow.ApproverRequirement
+            ApproverResources   = $workflow.ApproverResources
+            Description         = $newDescription
+            FixedDuration       = $workflow.FixedDuration
+            IconColor           = $workflow.IconColor
+            IconUrl             = $workflow.IconUrl
+            MultiSelectDuration = $workflow.MultiSelectDuration
+            Name                = $newName
+            NonAdminApproval    = $workflow.NonAdminApproval
+            ResourceId          = $workflow.ResourceId
+            SlackConfig         = $workflow.SlackConfig
+            SlackEnabled        = $workflow.SlackEnabled
+            Status              = $workflow.Status
+            TimeBasedAccess     = $workflow.TimeBasedAccess
+            TtlConfig           = $workflow.TtlConfig
+            VisibleTo           = $workflow.VisibleTo
+        }
+
+        foreach ($key in @($body.Keys)) {
+            if ($null -eq $body[$key]) {
+                $body.Remove($key) | Out-Null
+            }
+        }
+
+        $updatedWorkflow = Set-JcSdkApprovalFlow -ApprovalFlowId $workflow.Id -Body $body
+
+        $updatedWorkflow | Should -Not -BeNullOrEmpty
+
+        $refreshedWorkflow = Get-JcSdkApprovalFlow -ApprovalFlowId $workflow.Id
+        $refreshedWorkflow | Should -Not -BeNullOrEmpty
+        $refreshedWorkflow.Name | Should -Be $newName
+        $refreshedWorkflow.Description | Should -Be $newDescription
     }
 }
