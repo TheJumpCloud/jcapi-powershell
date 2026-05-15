2f37f4e403e0c171c4a4ddc162f4707cfc6d4dfb
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2026-05-15 10:53:49 -0600

    remove method name function for this pr

--- OpenAPI/scripts/OASFunctions.ps1
+++ OpenAPI/scripts/OASFunctions.ps1
@@ -61,38 +61,3 @@ Function Update-OasSpecMapping {
         }
     }
 }
-
-Function Add-OasSpecFunctionName {
-    Param(
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'Name of the API to build an SDK for.')][ValidateSet('JumpCloud.SDK.DirectoryInsights', 'JumpCloud.SDK.V1', 'JumpCloud.SDK.V2')][ValidateNotNullOrEmpty()][System.String[]]$SDKName
-    )
-
-    $SDKName | ForEach-Object {
-        # Get the mapping file for the specified SDKName
-        $mappingFile = Get-ChildItem -Path "$PSScriptRoot/../OAS/mapping" -Filter "OASMapping.$($SDKName).json"
-        $mappingFileContent = Get-Content -Path $mappingFile.FullName | ConvertFrom-Json -Depth 99 -AsHashtable
-
-        # Get the OAS Spec for the specified SDKName
-        $oasSpecFile = Get-ChildItem -Path "$PSScriptRoot/../OAS" -Filter "$SDKName.json"
-        $oasSpecFileContent = Get-Content -Path $oasSpecFile.FullName | ConvertFrom-Json -Depth 99
-
-        # Loop through the mapping file content and add the functionName to the OAS Spec
-        $mappingFileContent.GetEnumerator() | ForEach-Object {
-            $operationID = $_.Key
-            $path = $_.Value.path
-            $method = $_.Value.method
-            $functionName = $_.Value.'x-powershell-method-name'
-
-            # Check to see if the x-powershell-method-name already matches, if it doesn't change or add it to the OAS Spec
-            if ($oasSpecFileContent.paths.$path.$method.'x-powershell-method-name' -ne $functionName) {
-                Write-Host "[status] Adding x-powershell-method-name $functionName for $($operationID)"
-                $oasSpecFileContent.paths.$path.$method | Add-Member -MemberType NoteProperty -Name 'x-powershell-method-name' -Value $functionName -Force
-            } else {
-                Write-Verbose "[status] x-powershell-method-name $functionName already exists for $($operationID)"
-            }
-        }
-
-        # Save the updated OAS Spec
-        $oasSpecFileContent | ConvertTo-Json -Depth 99 | Set-Content -Path $oasSpecFile.FullName
-    }
-}
